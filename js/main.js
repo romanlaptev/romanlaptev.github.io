cddd0fdef31c945c1960cafdc09943ebefc76abb
romanlaptev <roman-laptev@yandex.ru>
2016-05-29 13:51:41 +0600

    edit frontpage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 ﻿window.onload = function(){
-console.log("onload");
+//console.log("onload");
+
+	//get time
+	var now = new Date(); 
+	var month = now.getMonth()+1;
+	if( month < 10){
+		month = "0" + month;
+	}
+	current_date.innerHTML = now.getDate()+ "." + month +"."+now.getFullYear();
+	//var specificDate = new Date(2016,4,28);//28.05.2016
+	
+	
 };//end load
