c9dd54af18b9798cba66f86922b31bd0263f040a
Roman Laptev <roman-laptev@yandex.ru>
2016-08-05 22:24:30 +0700

    test java console

---
+++ test_code/java/SimpleConsoleApp/SimpleConsoleApp.java
@@ -0,0 +1,15 @@
+//http://www.frolov-lib.ru/programming/javasamples/vol2/vol2_1/index.html
+//https://habrahabr.ru/post/125210/
+public class SimpleConsoleApp{
+	String szAppName;
+	
+	//constructor
+	//public SimpleConsoleApp(){
+		//szAppName = new String ("Simple Console Application");
+	//}
+	
+	public static void main( String args[] ){
+		System.out.println("Simple Console Application");
+	}
+	
+}
---
+++ test_code/java/SimpleConsoleApp/compile.cmd
@@ -0,0 +1,4 @@
+rem javac SimpleConsoleApp.java
+
+rem Run
+java -classpath . SimpleConsoleApp
