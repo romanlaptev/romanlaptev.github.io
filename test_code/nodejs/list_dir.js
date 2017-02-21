b88b9f9d031fb57c930f4858d8013598324f1cd0
Roman <roman-laptev@yandex.ru>
2017-02-21 11:13:52 +0700

    Update list_dir.js

--- test_code/nodejs/list_dir.js
+++ test_code/nodejs/list_dir.js
@@ -1,6 +1,17 @@
 //list directory
+//node list_dir.js d:\
+
+console.log( process.argv );
+var dir = ".";
+//var dir = "D:\\documents\\laptev\\www\\test\\";
+
+if( process.argv[2]){
+console.log ( process.argv[2] );
+var dir = process.argv[2];
+}
+
 var fs = require("fs");
-var files = fs.readdirSync(".");
+var files = fs.readdirSync( dir );
 for( fn in files){
 console.log( fn, files[fn] );
 }
