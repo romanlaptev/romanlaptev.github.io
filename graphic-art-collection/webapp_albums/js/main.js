202ffc981fad17be3dd2404312ffc075541f9bcf
roman <roman-laptev@yandex.ru>
2017-02-26 20:30:30 +0700

    remove cms
    Signed-off-by: roman <roman-laptev@yandex.ru>

--- graphic-art-collection/webapp_albums/js/main.js
+++
@@ -1,9 +0,0 @@
-var albums = Albums();
-console.log ("albums = ", albums);
-
-$(document).ready(function()	{
-
-	$("body").css("display","none").fadeIn(1000);
-
-});//end ready
-
--- music/webapp_music/api/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-//phpinfo();
-echo 2+2;
-?>
