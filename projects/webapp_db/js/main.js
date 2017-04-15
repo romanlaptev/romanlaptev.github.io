b31b0a9170ae6338c80e5c0679b23e4adc15c065
roman <roman-laptev@yandex.ru>
2017-04-15 14:38:52 +0700

    webapp_db

--- projects/webapp_db/js/main.js
+++ projects/webapp_db/js/main.js
@@ -1,186 +1,86 @@
-﻿
-var _vars = [];
-_vars["db_url"] = "db/art.xml";
-//_vars["db_url"] = "db/art.json";
-//_vars["db_url"] = "db/art.csv";
-
-_vars["db_type"] = "xml";
-
-_init();
-
-function _init(){
-console.log(_vars);
-
-	var url = _vars["db_url"];
-	_load( url, _postFunc );
+﻿var webApp = {
 	
-}//end init()	
-
-
-function _postFunc( data ){
+	"vars" : {
+		"log" : [],
+		// "templates" : {
+			// "subfolder_tpl" : "",
+			// "file_tpl" : ""
+		// },
+	"db_url" : "db/art.xml",
+	"db_type" : "xml"
+	//"db_url" :"db/art.json",
+	//"db_url" : "db/art.csv"
+	},
 	
-var msg = "load " + _vars["db_url"];
-console.log( msg );
-_log(msg);
+	"init" : function(){
+console.log("init webapp!");
+//console.log(webApp.vars);
 
-console.log( data );
+		runAjax( {
+			"requestMethod" : "GET", 
+			"url" : webApp.vars["db_url"], 
+			"callback": function( data ){
+var msg = "load " + webApp.vars["db_url"] ;
+console.log("<br>" + msg);
+//console.log( "_postFunc(), " + typeof data );
 
-}//end _postFunc()
-
-
-
-function _load( url, callback ){
-	
-	var xhr = createRequestObject();
-	if ( !xhr ) {
-console.log("error, ", xhr);
-		return false;
-	}
-	
-	var timeStart = new Date();
-	var async = true;
-	xhr.open('GET', url, async);
-	
-	xhr.onreadystatechange  = function() { 
-//console.log("state:", xhr.readyState);
-		if( xhr.readyState == 4) {
-console.log( xhr );
-console.log("end request, state " + xhr.readyState, ", status: " + xhr.status);
-				if( xhr.status === 200){
-					
-//console.log(xhr.getResponseHeader('X-Powered-By') );
-					var all_headers = xhr.getAllResponseHeaders();
-console.log( all_headers );
-					
-				var timeEnd = new Date();
-				var runtime = (timeEnd.getTime() - timeStart.getTime()) / 1000;
-var msg = "ajax load url: " + url + ", runtime: " + runtime +" sec";
-_log(msg);
-					
-//console.log( xhr.responseText );
-//console.log( xhr.responseXML );
-
-					if( typeof callback === "function"){
-						
-						if( xhr.responseXML ){
-							var data = xhr.responseXML;
-						} else {
-							var data = xhr.responseText;
-						}
-						callback(data);
-					}
-					
-				} else {
-console.log(xhr);					
-_log("<p>Ajax load error, url: <b class='text-danger'>" + xhr.responseURL + "</b></p>");
-_log("<p>Ajax load error, status: <b class='text-danger'>" + xhr.status + "</b></p>");
-_log("<p>Ajax load error, statusText: <b class='text-danger'>" + xhr.statusText + "</b></p>");
-				}
+				_dBase().init({
+					"data" : data,
+					"format" : webApp.vars["db_type"]
+				});
 				
-		}
-	};
+			}//end callback()
+		});
+		
+		//this.dBase();
+	}//end init()
 	
-	// xhr.onabort = function(){
-// _log("ajax onabort");
-// console.log(arguments);
-	// }
-	
-	// xhr.onerror = function(){
-// _log("ajax onerror");
-// console.log(arguments);
-	// }
-	
-	// xhr.onload = function(){
-// _log("ajax onload");
-// console.log(arguments);
-	// }
-	
-	// xhr.onloadstart = function(){
-// _log("ajax onloadstart");
-// console.log(arguments);
-	// }
-	
-	// xhr.onprogress = function(){
-// _log("ajax onprogress");
-// console.log(arguments);
-	// }
-	xhr.send();
+};//end webApp()
 
-	function createRequestObject() {
-		var request = false;
-		if (window.XMLHttpRequest) 
-		{ // Mozilla, Safari, Opera ...
-			request = new XMLHttpRequest();
-		} 
-
-		if(!request)
-		{ // IE
-			request = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-
-		if(!request)
-		{
-			request=new ActiveXObject('Msxml2.XMLHTTP');
-		}
-
-		return request;
-	}//end createRequestObject()
-	
-}//end load()
+webApp.init();
 
 
-var _dBase = function( options ){
-console.log(arguments);	
+function _dBase( opt ){
+//console.log(arguments);	
 
 	// private variables and functions
-		var _init = function(){
-console.log("init!!!");
-	};
-		
-	// init vars
-	var vars = {
-		"log" : [],
-		"templates" : {
-			"subfolder_tpl" : "",
-			"file_tpl" : "",
-		},
-		"testUrlPHP": "api/test.php",
-		"testUrlASPX": "api/aspx/test.aspx",
-		"music_alias" : "/music/",
-		"content_location" : "",
-		
-		//"protocol" : "file://",
-		//"protocol" : "",
-		
-		"dirname" : "",
-		"filelist_url" : "",
-		"copy_url" : "",
-		"rename_url" : "",
-		"remove_url" : "",
-		"mkdir_url" : "",
-		"save_pls_url" : "",
-		"filelist_src" : "",
-		"text_new_playlist" : "new playlist",
-		"playlist" : ""
-	};
-//console.log( "vars:" , vars );
+	var vars = {};
 
-	var _build = function(target){
-		var html = "Table " + target + " is building....";
-		return html;
+	var _init = function(args){
+console.log("init _dBase!", args);
+		vars["xmlData"] = args["data"];
+//console.log( "vars:" , vars["xmlData"].children );
+
+		// var xml = vars["xmlData"].children;
+// console.log( xml ) ;
+		// for( var item in xml){
+// console.log( item +" : "+ xml["item"] ) ;
+		// }
+		var xml = vars["xmlData"].getElementsByTagName("database");
+		var records = xml.item(0).getElementsByTagName("table");
+		
+console.log( records.length ) ;
+
 	};
+
+	var _query = function(args){
+		var data = 1;
+		return data;
+	};
+	
 
 	// public interfaces
 	return{
-		init:	function(){ 
-			return _init(); 
+		init:	function(args){ 
+//console.log(arguments);
+			return _init(args); 
 		},
-		build:	function(target){ 
-			return _build(target); 
+		query:	function(args){ 
+			return _query(args); 
 		}
 	};
-
 }//end _dBase()
+
 
 /*
 //Module MusicFM
@@ -211,11 +111,3 @@ console.log("init!!!");
 	MusicFM().init();
 })();
 */
-
-//window.dBase = _dBase;
-//console.log ("dBase = ",  dBase);
-
-_dBase().init();
-
-var test = _dBase().build("table1");
-_log (test);
--- projects/webapp_db/js/shared_functions.js
+++ projects/webapp_db/js/shared_functions.js
@@ -14,27 +14,35 @@ if (!window.console){
 };
 
 
-function _log( msg, id ){
-	var id =  id || "log";
-	
-	if( document.getElementById(id) ){
-		if( msg.length === 0){
-			document.getElementById(id).innerHTML = "";
-		} else {
-			document.getElementById(id).innerHTML += "<p>" + msg + "</p>";
-		}
+function _log( msg, id){
+//console.log(arguments);
+//alert(arguments.length);
+//		for( var n = 0; n < arguments.length; n++){
+//			var _s = "<li> arguments." + n +" = "+ arguments[n] + "</li>";
+//alert( _s );
+//		}
+	var id = id || arguments[1];//IE4 fix
+//alert( msg );
+//alert( id );
+
+	if(!id){
+		var id = "log";
 	}
 	
-	if(console && console.log) console.log(msg);
+	var output = getDOMobj(id);
+	if( output ){
 	
-	if (!window.console){ 
-		console = {
-			log: function( msg ) {
-				alert( msg );
-			}
+		if( msg.length == 0){
+			output.innerHTML = "";
+		} else {
+			output.innerHTML += msg;
 		}
-	};
-}//end _log
+		
+	} else {
+		alert(msg);
+		//document.writeln(msg);
+	}
+}//end _log()
 
 function getDOMobj(id){
 	
@@ -61,114 +69,6 @@ function getDOMobj(id){
 	return false;
 }//end getDOMobj()
 
-
-var _set_timer = function (){
-	var d = new Date;
-	return d.getTime();
-};
-var _get_timer = function (timer){
-	var d = new Date;
-	return parseFloat((d.getTime() - timer)/1000);
-};
-
-
-/*
-	var item_attr = get_attr_to_obj( this.attributes );
-	for(attr in item_attr){
-		column_obj[attr] = item_attr[attr];
-	}
-*/
-function get_attr_to_obj( attr ){
-	var item_attr = {};
-	for(var item = 0; item < attr.length; item++) {
-		item_attr[attr[item].name] = attr[item].value;
-	}
-	return item_attr;
-}//end get_attr_to_obj()
-
-
-//print source code
-//var source_txt = document.getElementById("code1");
-//code1_out.innerHTML +="<br><br>";
-//code1_out.appendChild( document.createTextNode( source_txt.outerHTML ) );
-
-
-//Мышь: IE8-, исправление события
-//https://learn.javascript.ru/fixevent
-/*
-elem.onclick = function(event) {
-  // если IE8-, то получить объект события window.event и исправить его
-  event = event || fixEvent.call(this, window.event);
-  ...
-}
-*/
-function fixEvent(e) {
-	e.currentTarget = this;
-	e.target = e.srcElement;
-
-	if (e.type == 'mouseover' || e.type == 'mouseenter') e.relatedTarget = e.fromElement;
-	if (e.type == 'mouseout' || e.type == 'mouseleave') e.relatedTarget = e.toElement;
-
-	if (e.pageX == null && e.clientX != null) {
-		var html = document.documentElement;
-		var body = document.body;
-
-		e.pageX = e.clientX + (html.scrollLeft || body && body.scrollLeft || 0);
-		e.pageX -= html.clientLeft || 0;
-
-		e.pageY = e.clientY + (html.scrollTop || body && body.scrollTop || 0);
-		e.pageY -= html.clientTop || 0;
-	}
-
-	if (!e.which && e.button) {
-		e.which = e.button & 1 ? 1 : (e.button & 2 ? 3 : (e.button & 4 ? 2 : 0));
-	}
-
-	return e;
-}
-
-
-//**************************************
-//var dirname = getenv("dirname");
-//**************************************
-function getenv(i){
-	if (!i.length) 
-	{ 
-		return false; 
-	}  
-	qStr = document.location.href;
-	strpos = qStr.indexOf("?"+i+"=");
-
-	if ( strpos ==-1) 
-	{ 
-		strpos = qStr.indexOf("&"+i+"="); 
-	}
-
-	if ( strpos == qStr.length || strpos ==-1 )
-	{
-		return false; 
-	}
-
-	val = qStr.substring( (strpos+i.length)+2, qStr.length);
-
-	strpos = val.indexOf("&");
-
-	if ( strpos !=-1 ) 
-	{ 
-		val = val.substring(0, strpos ); 
-	}
-
-	if ( !val.length ) 
-	{ 
-		return false; 
-	}
-	else 
-	{ 
-		return val; 
-	}
-
-}//end getenv
-
 //**************************************
 //$_GET = parseGetParams(); 
 //console.log( $_GET);
@@ -193,111 +93,6 @@ function parseGetParams() {
 }//end parseGetParams() 
 
 
-function detectBrowsers(){
-
-	var out = navigator.userAgent+"\n\r";
-	var isiPhone = navigator.userAgent.toLowerCase().indexOf("iphone");
-	var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
-	var isiPod = navigator.userAgent.toLowerCase().indexOf("ipod");
-	var Chrome = navigator.userAgent.toLowerCase().indexOf("chrome");
-	var Firefox = navigator.userAgent.toLowerCase().indexOf("firefox");
-	  if(isiPhone > -1){
-			out += "iPhone detect\n\r";
-			$("body").addClass("iphone");
-		   var iHeight = window.screen.height;
-		   if(iHeight <= 480) {
-			  out += 'iPhone 2 or iPhone 3 or iPhone 3GS\n\r';
-		   }
-		   else if(iHeight > 480 && iHeight <=960) {
-			   out += 'iPhone 4\n\r';
-		   }
-		   else if(iHeight > 960) {
-			  out += 'iPhone 5\n\r';
-		   }
-	  }
-	  if(isiPad > -1) {
-			out += "iPad detect\n\r";
-	  }
-	  if(isiPod > -1) {
-			out = "iPod detect\n\r";
-	  }
-	  if( Chrome > -1) {
-			out += "Chrome detect\n\r";
-			$("body").addClass("chrome");
-	  }
-	  if( Firefox > -1) {
-			out += "Firefox detect\n\r";
-			$("body").addClass("firefox");
-	  }
-
-}//end detectBrowsers
-
-	/*
-		if ((navigator.appName == "Microsoft Internet Explorer"))
-		{
-			if(navigator.userAgent.indexOf("MSIE 9")!=-1)
-					{
-					..............
-			}
-		}
-		var ua = navigator.userAgent.toLowerCase();
-		if (ua.indexOf("msie 9.0") != -1) {
-		}
-
-		// Gecko = Mozilla + Firefox + Netscape
-		if (ua.indexOf("gecko") != -1) {
-		}
-	*/
-
-function getNameBrowser() {
-	var ua = navigator.userAgent.toLowerCase();
-	// Internet Explorer
-	if (ua.indexOf("msie") != -1 && ua.indexOf("opera") == -1 && ua.indexOf("webtv") == -1) {
-		return "msie"
-	}
-	// Opera
-	if (ua.indexOf("opera") != -1) {
-		return "opera"
-	}
-	// Gecko = Mozilla + Firefox + Netscape
-	if (ua.indexOf("gecko") != -1) {
-		return "gecko";
-	}
-	// Safari, используется в MAC OS
-	if (ua.indexOf("safari") != -1) {
-		return "safari";
-	}
-	// Konqueror, используется в UNIX-системах
-	if (ua.indexOf("konqueror") != -1) {
-		return "konqueror";
-	}
-	return "unknown";
-}//end getNameBrowser
-
-
-function createRequestObject() {
-	var request = false;
-	if (window.XMLHttpRequest) 
-	{ // Mozilla, Safari, Opera ...
-		request = new XMLHttpRequest();
-	} 
-
-	if(!request)
-	{ // IE
-		request = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
-	if(!request)
-	{
-		request=new ActiveXObject('Msxml2.XMLHTTP');
-	}
-
-	return request;
-}
-
- //**************************************
-//  создать объект XMLHttpRequest
- //**************************************
 function getXMLDocument(url)  {  
 	var xml;  
 	if(window.XMLHttpRequest) {  
@@ -319,29 +114,68 @@ function getXMLDocument(url)  {
 	}
 }//end getXMLDocument
 
-//_loadAjax( url, _postFunc );
-function _loadAjax( url, callback ){
+
+/*
+	runAjax( {
+		"requestMethod" : "GET", 
+		"url" : _vars["db_url"], 
+		"callback": _postFunc
+	});
+*/
+function runAjax( opt ){
+//console.log(arguments);
 	
-	var xhr = createRequestObject();
+	var options = {
+		"requestMethod" : "GET", 
+		"url" : false, 
+		"params": "",
+		"async" :  true,
+		"callback" : null
+	};
+	//extend options object
+	for(var key in opt ){
+		options[key] = opt[key];
+	}
+//console.log(options);
+
+	var requestMethod = options["requestMethod"]; 
+	var url = options["url"]; 
+	var async = options["async"]; 
+	var callback = options["callback"]; 
+	
+	if( !url || url.length === 0){
+		var msg = "Parameters error, needed 'url'";			
+console.log( msg );
+//_log( "<p  class='text-danger'>" +msg+"</p>");
+		return false;
+	}
+	
+	var xhr = _createRequestObject();
 	if ( !xhr ) {
 console.log("error, ", xhr);
+		var msg = "_createRequestObject() error";			
+console.log( msg, xhr );
+//_log( "<p  class='text-danger'>" +msg+"</p>");
 		return false;
 	}
 	
 	var timeStart = new Date();
-	var async = true;
-	xhr.open('GET', url, async);
 	
+	xhr.open( requestMethod, url, async );
 	xhr.onreadystatechange  = function() { 
 //console.log("state:", xhr.readyState);
 		if( xhr.readyState == 4) {
-console.log( xhr );
-console.log("end request, state " + xhr.readyState, ", status: " + xhr.status);
+//console.log( xhr  );
+//for(var key in xhr){
+//console.log( key +" : "+xhr[key] );
+//}
+
+//console.log("end request, state " + xhr.readyState + ", status: " + xhr.status);
 				if( xhr.status === 200){
 					
 //console.log(xhr.getResponseHeader('X-Powered-By') );
 					var all_headers = xhr.getAllResponseHeaders();
-console.log( all_headers );
+//console.log( all_headers );
 					
 				var timeEnd = new Date();
 				var runtime = (timeEnd.getTime() - timeStart.getTime()) / 1000;
@@ -349,7 +183,17 @@ console.log( all_headers );
 				_log(msg);
 					
 //console.log( xhr.responseText );
-console.log( xhr.responseXML );
+//console.log( xhr.responseXML );
+					if( typeof callback === "function"){
+						
+						if( xhr.responseXML ){
+							var data = xhr.responseXML;
+						} else {
+							var data = xhr.responseText;
+						}
+						callback(data);
+					}
+
 				} else {
 console.log(xhr);					
 _log("<p>Ajax load error, url: <b class='text-danger'>" + xhr.responseURL + "</b></p>");
@@ -360,30 +204,49 @@ _log("<p>Ajax load error, statusText: <b class='text-danger'>" + xhr.statusText 
 		}
 	};
 	
-	xhr.onabort = function(){
-_log("ajax onabort");
-console.log(arguments);
-	}
-	
-	xhr.onerror = function(){
-_log("ajax onerror");
-console.log(arguments);
-	}
-	
-	xhr.onload = function(){
-_log("ajax onload");
-console.log(arguments);
-	}
-	
-	xhr.onloadstart = function(){
-_log("ajax onloadstart");
-console.log(arguments);
-	}
-	
-	xhr.onprogress = function(){
-_log("ajax onprogress");
-console.log(arguments);
-	}
+	// if( xhr.onabort ){
+		// xhr.onabort = function(){
+// _log("ajax onabort");
+// //console.log(arguments);
+		// }
+	// }
+// console.log( "xhr.onabort " + xhr.onabort  );
+
+	// if( xhr.onerror ){
+		// xhr.onerror = function(){
+// _log("ajax onerror");
+// console.log(arguments);
+// console.log(xhr);					
+// _log("<p>Ajax load error, url: <b class='text-danger'>" + xhr.responseURL + "</b></p>");
+// _log("<p>Ajax load error, status: <b class='text-danger'>" + xhr.status + "</b></p>");
+// _log("<p>Ajax load error, statusText: <b class='text-danger'>" + xhr.statusText + "</b></p>");
+		// }
+	// }
+// console.log( "xhr.onerror " + xhr.onerror  );
+
+	// if( xhr.onload ){
+		// xhr.onload = function(){
+// _log("ajax onload");
+// console.log(arguments);
+		// }
+	// }
+// console.log( "xhr.onload " + xhr.onload  );
+
+	// if( xhr.onloadstart ){
+		// xhr.onloadstart = function(){
+// _log("ajax onloadstart");
+// console.log(arguments);
+		// }
+	// }
+// console.log( "xhr.onloadstart " + xhr.onloadstart  );
+
+	// if( xhr.onprogress ){
+		// xhr.onprogress = function(){
+// _log("ajax onprogress");
+// console.log(arguments);
+		// }
+	// }
+// console.log( "xhr.onprogress " + xhr.onprogress  );
 
 	// if (xhr.setRequestHeader) {
 		// xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
@@ -392,287 +255,34 @@ console.log(arguments);
 	//xhr.send(str);
 	xhr.send();
 
-	function createRequestObject() {
+	function _createRequestObject() {
 		var request = false;
-		if (window.XMLHttpRequest) 
-		{ // Mozilla, Safari, Opera ...
+		
+		if (window.XMLHttpRequest) { // Mozilla, Safari, Opera ...
 			request = new XMLHttpRequest();
 		} 
 
-		if(!request)
-		{ // IE
+		if(!request){ // IE
+//console.log("use Microsoft.XMLHTTP");		
 			request = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 
-		if(!request)
-		{
+		if(!request){
+//console.log("use Msxml2.XMLHTTP");		
 			request=new ActiveXObject('Msxml2.XMLHTTP');
 		}
 
 		return request;
-	}//end createRequestObject()
+	}//end _createRequestObject()
 	
-}//end loadAjax()
-
-
-// фильтрация ввода, только цифры
-function filter_input(e,regexp){
-  e=e || window.event;
-  var target=e.target || e.srcElement;
-  var isIE=document.all;
-
-  if (target.tagName.toUpperCase()=='INPUT')
-  {
-    var code=isIE ? e.keyCode : e.which;
-    if (code<32 || e.ctrlKey || e.altKey) return true;
-
-    var char=String.fromCharCode(code);
-    if (!regexp.test(char)) return false;
-  }
-  return true;
-}
-
-function check_form(){
-//console.log(document.forms.sendform[0].value);
-	var error = true;
-	var error_text = "";
-	var frm = document.forms.sendform;
-	for (item in frm.elements) {
-		if (item == "email")
-		{
-//console.log(frm.elements[item].value);
-			var email_value = frm.elements[item].value;
-			if (email_value.length > 0)
-			{
-// /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/
-				var reg = /^\w+([\.-]?\w+)*@\w+([\.-]\w+)*(\.\w{2,4})+$/;
-				if (email_value.search(reg) !=-1 )
-				{
-					error = false;
-				}
-
-			} else {
-				error_text += "enter email";
-				//frm.elements[item].styles="enter email!!!";
-			}
-		}
-	}
-
-	if (!error){
-		document.forms.sendform.submit();
-//return false;
-	} else {
-		alert ('Error: ' + error_text);
-	}
-}//end check_form()
-
+}//end runAjax()
 
 
 if( typeof window.jQuery === "function"){
-//msg = 'You are running jQuery version: ' + jQuery.fn.jquery;
+var msg = 'You are running jQuery version: ' + jQuery.fn.jquery;
+_log(msg);
 	$(document).ready(function(){
-		
-		//ввод только цифр
-		$('.only-numbers').on('keydown', function(event) {
-//console.log("event.keyCode = " + event.keyCode );
-			if (event.keyCode == 13) {
-				return;
-			}
-
-			if ( event.keyCode == 46 || 
-				event.keyCode == 8 || 
-				event.keyCode == 9 || 
-				event.keyCode == 27 ||
-					(event.keyCode == 65 && event.ctrlKey === true) ||
-						(event.keyCode >= 35 && event.keyCode <= 39)
-			) {
-				return;
-			} else {
-				if ( (event.keyCode < 48 || event.keyCode > 57) &&
-					(event.keyCode < 96 || event.keyCode > 105 )
-				) {
-					event.preventDefault();
-				}
-			}
-		});
-		
-		//------------------------- scroll to top
-		$("#scroll-to-top").click(function(e) {
-			e.preventDefault;
-			$('html,body').animate({
-				scrollTop: 0
-				}, 500);
-			return false;
-		});
-		
-		$(".scroll-to").addClass("nolink").on("click", function(){
-			if($(this).attr("href")){
-			var elem = $(this).attr("href");
-			}else{
-			var elem = $(this).attr("data-target");
-			}
-			$('body').scrollTo( elem, 800, {offset: -50});
-			return false;
-		});
-		
-		$(".fancybox").fancybox({
-			helpers : {
-				overlay : {
-					locked : false
-				}
-			}
-		});
 		
 	});//end ready	
 }
 
-
-// Вывод всех элементов формы
-function print_forms() {
-	var frm = document.form_ls;
-	for ( var n2=1; n2 < frm.elements.length; n2++)
-	   {
-		var elmnt = frm.elements[n2];
-		document.write ("element " + n2 + "= " + elmnt.name+", ");
-		document.write (elmnt.type + ", ");
-		document.write (elmnt.value+"<br>");
-	   }
-}//end print_forms()
-
-//Dump for object
-/*  
-print_f (cell_hover_top.style); //Dump IE styles
-.......
-textDecorationBlink
-scrollbarFaceColor
-.......
-*/
-function print_f( id ){
-	var str = '';
-   if(typeof(id) == "object"){
-      for(a in id){
-         str += (a +"<br>");   
-      }   
-   }
-	document.write (str);
-}//end
-
-function select_checkbox() {
-   var frm = document.form_ls;
-   for (var n1=1; n1 < frm.elements.length; n1++)
-      {
-        var elmnt = frm.elements[n1];
-        if (elmnt.type == 'checkbox')
-          {
-            elmnt.checked = true;
-          }
-      }
- }//end function
-
-function select_change_action() {
-   var num = 0;
-   var a = '';
-   num = document.forms.form_ls.change_action.selectedIndex;
-   a = document.forms.form_ls.change_action[num].value;
-//   window.alert (a);
-}//end 
- 
-//-----------------------------------------------------------
-// очистить помеченные checkbox
-//-----------------------------------------------------------
-function clear_checkbox (){
-      var frm = document.form_ls;
-      for ( var n2=1; n2 < frm.elements.length; n2++)
-         {
-          var elmnt = frm.elements[n2];
-          if  (elmnt.type=='checkbox') 
-            {
-              elmnt.checked = false;
-            }
-         }
-}//end
-
-//-----------------------------------------------------------
-// слои
-//-----------------------------------------------------------
-  function init() {
-     IE = (document.all)
-     NC = (document.layers)
-     Opera = (document.getElementById)
-   }
-
-  function hiddenLayer(filename)  {
-    init();
-    if (IE) eval('document.all["desc"].style.visibility = "hidden"')
-    if (NC) eval('document.layers["desc"].visibility = "hidden"')
-    if (Opera) eval('document.getElementById("desc").style.visibility = "hidden"')
-   }
-
-  function showLayer(filename) {
-    init();
-    if (IE) eval('document.all["desc"].style.visibility = "visible"')
-    if (NC) eval('document.layers["desc"].visibility = "visible"')
-    if (Opera) eval('document.getElementById("desc").style.visibility = "visible"')
-   }
-
-  function processnode111(nnodeid)
-   {
-    if (document.getElementById("div_" + nnodeid).style.display == "none")
-      {
-      document.getElementById("div_" + nnodeid).style.display = ""
-      }
-    else
-      {
-      document.getElementById("div_" + nnodeid).style.display = "none"
-      }
-   }
-
-  function processnode(nnodeid)
-   {
-    if (document.getElementById(nnodeid).style.display == "none")
-      {
-      document.getElementById(nnodeid).style.display = ""
-      }
-    else
-      {
-      document.getElementById(nnodeid).style.display = "none"
-      }
-   }
-
-   
-function set_cookie(name, value, expires){
-alert(name);
-	if (!expires){
-		expires = new Date();
-	}
-//http://javascript.ru/date/togmtstring	
-	document.cookie = name + "=" + escape(value) + "; expires=" + expires.toGMTString() +  "; path=/";
-}//end set_cookie
-
-
-function convertTimestamp(timestamp) {
-	var d = new Date(timestamp),	// Convert the passed timestamp to milliseconds
-	yyyy = d.getFullYear(),
-	mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
-	dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
-	hh = d.getHours(),
-	h = hh,
-	min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
-	ampm = 'AM',
-	time;
-			
-	if (hh > 12) {
-		h = hh - 12;
-		ampm = 'PM';
-	} else if (hh === 12) {
-		h = 12;
-		ampm = 'PM';
-	} else if (hh == 0) {
-		h = 12;
-	}
-	
-	// ie: 2013-02-18, 8:35 AM	
-	time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
-		
-	return time;
-}//end convertTimestamp()
