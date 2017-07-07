a9e7ae9c912c7fcd049a2cbd8ffa83c6b188e57d
Roman <roman-laptev@yandex.ru>
2017-07-07 11:28:04 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,36 @@
 	
 	var day_in_ms = 1000*60*60*24;
 	var Days = Math.floor((now.getTime() - startDate.getTime())/ day_in_ms );
-	document.getElementById("uptime").innerHTML = Days;
+	//document.getElementById("uptime").innerHTML = Days;
+	document.getElementById("current_date").innerHTML += ", ("+Days+"), "; 
+	 
+//detect type year
+//год является високосным в двух случаях: либо он кратен 4, но при этом не кратен 100, либо кратен 400
+var year = now.getFullYear();
+//var year = 1600;
+var test1 = year % 4;
+//console.log(test1);
+if( test1 === 0){
+	test1 = true;
 	
+	var test2 = year % 400;
+	if( test2 > 0){
+		test1 = false;
+	}
+
+} else {
+	test1 = false;
+}
+if( test1){
+//console.log("год является високосным!");	
+	var numDaysInYear = 366;
+} else {
+//console.log("год не является високосным!");
+	var numDaysInYear = 365;
+}
+
+var numYears = Math.floor(Days / numDaysInYear);
+var numDays = Days % numDaysInYear;
+document.getElementById("uptime").innerHTML = numYears +" year "+numDays;
+	 
 };//end load
