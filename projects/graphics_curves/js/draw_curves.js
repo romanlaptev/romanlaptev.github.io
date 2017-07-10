d6071069e7bd5c2da89d1d54843ca45568157414
roman <roman-laptev@yandex.ru>
2017-07-10 19:48:31 +0700

    update graphics

--- projects/graphics_curves/js/draw_curves.js
+++ projects/graphics_curves/js/draw_curves.js
@@ -1,78 +1,86 @@
 ﻿var _vars = {
 	"graphics" : {
 		"sinus" : {
+			"canvasID" : "canvas-sin",
+			"formID" : "form1",
+			"context" : null,
 			"start_x" : 100,
 			"start_y" : 100,
 			"step" : 0.01,
 			"num_repeat" : 12,
 			"k1" : 30,
-			"k2" : 70
+			"k2" : 70//,
+			//"build" : sinus
 		}
 	}
 }
+console.log(_vars);
 
 window.onload = function(){
 
 		//get DOM objects
-		var canvasObj = getDOMobj( "canvas-sin" );
+		// var canvasID = _vars["graphics"]["sinus"]["canvasID"];
+		// var canvasObj = getDOMobj( canvasID );
 		
-		//draw graphic
-		try {
-			var context = canvasObj.getContext("2d");
-			context.fillStyle = 'blue';
-			var params = _vars["graphics"]["sinus"];
-			initForm();
-			sinus( context, params);
-		} catch (e) {
-			var msg = "HTML5 Canvas is not supported in this browser";
-			msg += "<br> "+e.name+ ", "+e.message+ ", "+e.number+ ", "+e.description;
-console.log(msg);			
-		}
+		// //draw graphic
+		// try {
+			// var context = canvasObj.getContext("2d");
+			// context.fillStyle = 'blue';
+			// var params = _vars["graphics"]["sinus"];
+			// initForm();
+			// sinus( context, params);
+		// } catch (e) {
+			// var msg = "HTML5 Canvas is not supported in this browser";
+			// msg += "<br> "+e.name+ ", "+e.message+ ", "+e.number+ ", "+e.description;
+// console.log(msg);			
+		// }
 
+		var params = _vars["graphics"]["sinus"];
+		params["context"] = _createCanvas( params["canvasID"] );
+		initForm( params["formID"] );
+		var context = params["context"];
+		sinus( context, params);
+		
 //=======================================		
 		//clear-btn
 		var btn_clear_canvas = document.querySelector(".clear-canvas");
 //console.log(btn_clear_canvas);		
 		btn_clear_canvas.addEventListener("click", function(e){
 			var canvas_id = e.target.getAttribute("data-target");
-console.log(canvas_id);			
-			var canvasObj = getDOMobj( canvas_id );
-			clear_canvas( canvasObj );
-			initForm();
+//console.log(canvas_id);			
+			clear_canvas( canvas_id );
+			initForm( _vars["graphics"]["sinus"]["formID"] );
 		}, false);//end event
-		
-//=======================================		
-		var startXRange = getDOMobj( "start-x-range" );
-//console.log( startXRange );		
-		var startXVal = getDOMobj( "start-x-val" );
-		startXVal.value = startXRange.value;				
-		// startXRange.oninput = function(e){
-// //console.log( e, this );
-			// startXVal.value=this.value;				
-		// }
-		startXRange.onchange = function(e){
-			startXVal.value=this.value;				
-		}
-	
-//=======================================		
-		var startYRange = getDOMobj( "start-y-range" );
-		var startYVal = getDOMobj( "start-y-val" );
-		startYVal.value = startYRange.value;				
-		startYRange.onchange = function(e){
-			startYVal.value=this.value;				
-		}
+
 	
 }//end load
 
+function _createCanvas( id ){
+	//get DOM objects
+	var canvasObj = getDOMobj( id );
+	
+	try {
+		var context = canvasObj.getContext("2d");
+		context.fillStyle = 'blue';
+		return context;
+	} catch (e) {
+		var msg = "HTML5 Canvas is not supported in this browser";
+		msg += "<br> "+e.name+ ", "+e.message+ ", "+e.number+ ", "+e.description;
+console.log(msg);	
+		return false;
+	}
+	
+}//end _createCanvas()
 
-function clear_canvas( canvas ){
-//console.log( "clear!!!", canvas.width, canvas.height );
-	var context = canvas.getContext("2d");
-	context.clearRect(0, 0, canvas.width, canvas.height);
+function clear_canvas( id ){
+	var canvasObj = getDOMobj( id );
+	var context = canvasObj.getContext("2d");
+	context.clearRect(0, 0, canvasObj.width, canvasObj.height);
 }//end clear_canvas
 
 
 function sinus( context, params ){
+console.log(params);	
 	//var p = {
 	//}
 	var start_x = params.start_x;
@@ -88,19 +96,46 @@ function sinus( context, params ){
 		x = start_x + ( Math.round(x_) ) ;
 		y = start_y + ( Math.round(y_) );
 		context.fillRect(x, y, 2, 2);
+	}//next
+	
+}//end sinus()
+
+
+function initForm( id ){
+	var form = getDOMobj( id );
+	
+	var startXRange = getDOMobj( "start-x-range" );
+	startXRange.value = _vars["graphics"]["sinus"]["start_x"];
+	var startXVal = getDOMobj( "start-x-val" );
+	startXVal.value = startXRange.value;				
+	//form.step.start_x_range.value = _vars["graphics"]["sinus"]["start_x"];
+	startXRange.onchange = function(e){
+		startXVal.value=this.value;				
 	}
 	
-	//define rebuild event
-	var form = getDOMobj( "form1" );
-//console.log( form.onsubmit );	
-//console.log( form.onreset );	
+	var startYRange = getDOMobj( "start-y-range" );
+	startYRange.value = _vars["graphics"]["sinus"]["start_y"];
+	var startYVal = getDOMobj( "start-y-val" );
+	startYVal.value = startYRange.value;				
+	startYRange.onchange = function(e){
+		startYVal.value=this.value;				
+	}
+	
+	//form.step.start_y_range.value = _vars["graphics"]["sinus"]["start_y"];
+	
+	form.num_repeat.value  = _vars["graphics"]["sinus"]["num_repeat"];
+	form.step.value  = _vars["graphics"]["sinus"]["step"];
+	form.k1.value = _vars["graphics"]["sinus"]["k1"];
+	form.k2.value = _vars["graphics"]["sinus"]["k2"];
+	
 	if( typeof form.onsubmit !== "function"){
 		form.onsubmit = function( event ){
+			
 			var canvas = document.querySelector("#canvas-sin");
 			var context = canvas.getContext("2d");
 			context.fillStyle = 'blue';
 			
-			var params = {
+			var p = {
 				start_x : parseInt( form.start_x.value ),
 				start_y : parseInt( form.start_y.value ),
 				step : parseFloat( form.step.value ),
@@ -109,7 +144,7 @@ function sinus( context, params ){
 				k2 : parseInt( form.k2.value )
 			};
 	//console.log("submit", params);
-			sinus( context, params);
+			sinus( context, p);
 			
 			event.preventDefault();
 		};
@@ -117,34 +152,11 @@ function sinus( context, params ){
 	}
 	if( typeof form.onreset !== "function"){
 		form.onreset = function( event ){
-			initForm();
+			initForm( _vars["graphics"]["sinus"]["formID"] );
 			event.preventDefault();
 		};
-		
 	}
-	
-}//end func
-
-
-function initForm(){
-	var form = getDOMobj( "form1" );
-	
-	var startXRange = getDOMobj( "start-x-range" );
-	startXRange.value = _vars["graphics"]["sinus"]["start_x"];
-	var startXVal = getDOMobj( "start-x-val" );
-	startXVal.value = startXRange.value;				
-	//form.step.start_x_range.value = _vars["graphics"]["sinus"]["start_x"];
-	
-	var startYRange = getDOMobj( "start-y-range" );
-	startYRange.value = _vars["graphics"]["sinus"]["start_y"];
-	var startYVal = getDOMobj( "start-y-val" );
-	startYVal.value = startYRange.value;				
-	
-	//form.step.start_y_range.value = _vars["graphics"]["sinus"]["start_y"];
-	
-	form.num_repeat.value  = _vars["graphics"]["sinus"]["num_repeat"];
-	form.step.value  = _vars["graphics"]["sinus"]["step"];
-	form.k1.value = _vars["graphics"]["sinus"]["k1"];
-	form.k2.value = _vars["graphics"]["sinus"]["k2"];
+//console.log( form.onsubmit );	
+//console.log( form.onreset );	
 	
 }//end initForm()
