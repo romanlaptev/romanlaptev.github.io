6ce04cdc48cc6df0e663288f515afc7f8e76bc60
Roman <roman-laptev@yandex.ru>
2017-07-25 09:57:26 +0700

    Update shared_functions.js

--- projects/webapp_db/js/shared_functions.js
+++ projects/webapp_db/js/shared_functions.js
@@ -241,7 +241,7 @@ _log("<p>Ajax load error, statusText: <b class='text-danger'>" + xhr.statusText 
 	// }
 // console.log( "xhr.onloadstart " + xhr.onloadstart  );
 
-	if( xhr.onprogress ){
+	//if( "onprogress" in xhr ){
 		xhr.onprogress = function(e){
 //_log("ajax onprogress");
 //console.log(arguments);
@@ -254,9 +254,10 @@ console.log( "Loaded " + e.loaded + " bytes of total " + e.total, e.lengthComput
 				document.getElementById("load-progress").value = percentComplete;
 			}
 		}
-	}
-// console.log( "xhr.onprogress " + xhr.onprogress  );
+	//}
+//console.log( "xhr.onprogress ", xhr.onprogress  );
 
+//console.log( "setRequestHeader" in xhr  );
 	// if (xhr.setRequestHeader) {
 		// xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 	// }
