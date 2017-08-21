2c7ad295f66acad5f036283d65d872982186f83e
Roman <roman@vbox1>
2017-08-21 14:32:38 +0900

    update notes

--- projects/notes/notes.php
+++ projects/notes/notes.php
@@ -10,9 +10,9 @@ ini_set('display_errors', 1);
 //echo "</pre>";
 
 $_vars=array();
-$_vars["config"]["host"] = "localhost";
-$_vars["config"]["user"] = "root";
-$_vars["config"]["password"] = "master";
+$_vars["config"]["dbHost"] = "localhost";
+$_vars["config"]["dbUser"] = "root";
+$_vars["config"]["dbPassword"] = "master";
 $_vars["config"]["dbName"] = "db1";
 
 //echo PHP_VERSION;
@@ -21,14 +21,20 @@ $_vars["config"]["dbName"] = "db1";
 $_vars["config"]["phpversion"] = phpversion();
 
 
-//$_vars["config"]["host"] = "mysql.hostinger.ru";
-//$_vars["config"]["user"] = "u380901270_usr";
-//$_vars["config"]["password"] = "E6bAsZYBs4";
+//$_vars["config"]["dbHost"] = "mysql.hostinger.ru";
+//$_vars["config"]["dbUser"] = "u380901270_usr";
+//$_vars["config"]["dbPassword"] = "E6bAsZYBs4";
 //$_vars["config"]["dbName"] = "u380901270_db1";
 
 $_vars["config"]["tableName"] = "notes";
 
-$_vars["sql"]["createDB"] = "CREATE DATABASE ".$_vars["config"]["dbName"]." DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+$_vars["sql"]["createDB"] = "CREATE DATABASE IF NOT EXISTS ".$_vars["config"]["dbName"]." DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+/*
+        $dbh->exec("CREATE DATABASE `$db`;
+                CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
+                GRANT ALL ON `$db`.* TO '$user'@'localhost';
+                FLUSH PRIVILEGES;") 
+*/
 $_vars["sql"]["createTable"] = "CREATE TABLE IF NOT EXISTS `".$_vars["config"]["tableName"]."` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `author` varchar(20) NOT NULL,
@@ -50,44 +56,25 @@ $_vars["sql"]["insertMessage"] = "INSERT INTO `".$_vars["config"]["tableName"]."
 $_vars["sql"]["showTables"] = "SHOW TABLES  FROM `".$_vars["config"]["dbName"]."`";
 $_vars["sql"]["getMessages"] = "SELECT id, author, title, text_message, client_date, server_date, ip FROM `".$_vars["config"]["tableName"]."`";
 $_vars["sql"]["deleteMessage"] = "DELETE FROM `".$_vars["config"]["tableName"]."` WHERE `id`={{id}}";
+$_vars["useMySQL"] = 1;
+$_vars["usePDO"] = 0;
 
 	$action = "";
 	if( !empty($_REQUEST['action']) ){
 		$action = $_REQUEST['action'];
 	}
-//========================================= start
-	//connect to server
-	$_vars["link"] = connectMySQL();
-//echo $_vars["link"];
-
-	//create database (IF NOT EXISTS)
-	$dbName = $_vars["config"]["dbName"];
-	$db = mysql_select_db($dbName, $_vars["link"]);
-	if (!$db){
-		$query = $_vars["sql"]["createDB"];
-		if (mysql_query($query, $_vars["link"]) ) {
-			//echo "base $dbName created succesfully<br>";
-			$db = mysql_select_db($dbName, $_vars["link"]);
-		} else {
-			echo "error create $dbName: " . mysql_error() . "<br>";
-			echo "SQL: " . $query . "<br>";
-			exit();
-		}				
-	}
-	
-	//create table (CREATE TABLE IF NOT EXISTS)
-	//$tableName = $_vars["config"]["tableName"];
-	$query = $_vars["sql"]["showTables"];
-	$tables = getDataRow( $query, $_vars["link"] );
-	$test = 0;
-	for( $n = 0; $n < count ($tables); $n++){
-		if( $tables[$n] == $_vars["config"]["tableName"]){
-			$test = 1;
-		}
-	}//next
-	if(!$test){
-		$query = $_vars["sql"]["createTable"];
-		mysql_query($query, $_vars["link"]);
+//========================================= connect to server
+	//check PDO support
+	if (!defined('PDO::ATTR_DRIVER_NAME')) {
+		$_vars["link"] = connectDbMySQL();
+		createDbMySQL();
+		createTableMySQL();
+	} else {
+		$_vars["useMySQL"] = 0;
+		$_vars["usePDO"] = 1;
+		$_vars["link"] = connectDbPDO();
+		createDbPDO();
+		createTablePDO();
 	}
 	
 	switch ($action){
@@ -110,23 +97,31 @@ $_vars["sql"]["deleteMessage"] = "DELETE FROM `".$_vars["config"]["tableName"]."
 		break;
 		
 	}//end switch
-	mysql_close ( $_vars["link"] );
+
+	
+	if($_vars["useMySQL"] == 1){
+		mysql_close ( $_vars["link"] );
+	}
+	if($_vars["usePDO"] == 1){
+		unset ($_vars["link"]);
+	}
+
 //=========================================== end	
 	
-function connectMySQL(){
+function connectDbMySQL(){
 	global $_vars;
 // echo "<pre>";
 // print_r($_vars);
 // echo "</pre>";
 
-	$host = $_vars["config"]["host"];
-	$user = $_vars["config"]["user"];
-	$password = $_vars["config"]["password"];
+	$dbHost = $_vars["config"]["dbHost"];
+	$dbUser = $_vars["config"]["dbUser"];
+	$dbPassword = $_vars["config"]["dbPassword"];
 	$dbName = $_vars["config"]["dbName"];
 	$tableName = $_vars["config"]["tableName"];
 	
 	try{
-		$link = mysql_connect($host, $user, $password);
+		$link = mysql_connect($dbHost, $dbUser, $dbPassword);
 		if (!$link){
 			throw new Exception('MySQL Connection Database Error: ' . mysql_error());
 		} else{
@@ -159,8 +154,101 @@ function connectMySQL(){
 		exit();
 	}
 
-}//end connectMySQL()
+}//end connectDbMySQL()
+
+function connectDbPDO(){
+	global $_vars;
+// echo "<pre>";
+// print_r($_vars);
+// echo "</pre>";
+
+	$dbHost = $_vars["config"]["dbHost"];
+	$dbUser = $_vars["config"]["dbUser"];
+	$dbPassword = $_vars["config"]["dbPassword"];
+	$dbName = $_vars["config"]["dbName"];
+	$tableName = $_vars["config"]["tableName"];
 	
+	$dsn = "mysql:host={$dbHost}";
+	try{
+		$connection = new PDO( $dsn, $dbUser, $dbPassword );
+		
+		$query = "set character_set_client='utf8'";
+		$connection->query( $query ) or die( print_r($connection->errorInfo(), true) );
+		
+		$query = "set character_set_results='utf8'";
+		$connection->query( $query ) or die( print_r($connection->errorInfo(), true) );
+		
+		$query = "set collation_connection='utf8_general_ci'";
+		$connection->query( $query ) or die( print_r($connection->errorInfo(), true) );
+		
+		return $connection;
+	} catch( PDOException $exception ) {
+		echo $exception->getMessage();
+		exit();
+	}
+	
+}//end connectDbPDO()
+
+
+//=========================================== create database (IF NOT EXISTS)
+function createDbMySQL(){
+	global $_vars;
+	
+	$dbName = $_vars["config"]["dbName"];
+	$db = mysql_select_db($dbName, $_vars["link"]);
+	if (!$db){
+		$query = $_vars["sql"]["createDB"];
+		if (mysql_query($query, $_vars["link"]) ) {
+			//echo "base $dbName created succesfully<br>";
+			$db = mysql_select_db($dbName, $_vars["link"]);
+		} else {
+			echo "error create $dbName: " . mysql_error() . "<br>";
+			echo "SQL: " . $query . "<br>";
+			exit();
+		}				
+	}
+}//end createDbMySQL()	
+
+function createDbPDO(){
+	global $_vars;
+	$query = $_vars["sql"]["createDB"];
+	$connection = $_vars["link"];
+	$connection->exec( $query ) or die( print_r($connection->errorInfo(), true) );
+	$connection->query("use ".$_vars["config"]["dbName"]);	
+}//end createDbPDO()	
+
+
+//======================================== create table (CREATE TABLE IF NOT EXISTS)
+function createTableMySQL(){
+	global $_vars;
+	
+	//$tableName = $_vars["config"]["tableName"];
+	// $query = $_vars["sql"]["showTables"];
+	// $tables = getDataRow( $query, $_vars["link"] );
+	// $test = 0;
+	// for( $n = 0; $n < count ($tables); $n++){
+		// if( $tables[$n] == $_vars["config"]["tableName"]){
+			// $test = 1;
+		// }
+	// }//next
+	// if(!$test){
+		// $query = $_vars["sql"]["createTable"];
+		// mysql_query($query, $_vars["link"]);
+	// }
+	$query = $_vars["sql"]["createTable"];
+	mysql_query($query, $_vars["link"]);
+}//end createTableMySQL()
+
+function createTablePDO(){
+	global $_vars;
+	
+	$query = $_vars["sql"]["createTable"];
+	$connection = $_vars["link"];
+	$connection->query( $query ) or die( print_r($connection->errorInfo(), true) );
+}//end createTablePDO()	
+
+
+
 function saveMessage(){
 	global $_vars;
 
@@ -170,7 +258,7 @@ function saveMessage(){
 	$clientDate = $_REQUEST["date"];
 	$serverDate = date(DATE_ATOM);
 	$ip = $_SERVER["REMOTE_ADDR"];
-
+	
 	$query = $_vars["sql"]["insertMessage"];
 	$query = str_replace("{{authorName}}", $authorName, $query);
 	$query = str_replace("{{title}}", $title, $query);
@@ -178,13 +266,55 @@ function saveMessage(){
 	$query = str_replace("{{client_date}}", $clientDate, $query);
 	$query = str_replace("{{server_date}}", $serverDate, $query);
 	$query = str_replace("{{ip}}", $ip, $query);
+
+	if($_vars["useMySQL"] == 1){
+
+		if (mysql_query($query, $_vars["link"]) ) {
+			echo "record was inserted...";
+		} else {
+			echo "error INSERT: " . mysql_error() . "<br>";
+			echo "SQL: " . $query;
+			exit();
+		}
+	}
 	
-	if (mysql_query($query, $_vars["link"]) ) {
-		echo "record was inserted....<br>";
-	} else {
-		echo "error INSERT: " . mysql_error() . "<br>";
-		echo "SQL: " . $query . "<br>";
-		exit();
+	if($_vars["usePDO"] == 1){
+		$connection = $_vars["link"];
+//Protection against sql-injections
+//1. 
+//$username = PDO::quote($_GET['username']);
+//$pdo->query("SELECT * FROM users WHERE username = $username");
+//2.
+//$pdo->prepare('SELECT * FROM users WHERE username = :username');
+//$pdo->execute(array(':username' => $_GET['username']));
+		// $query = str_replace("{{authorName}}", ":author", $query);
+		// $query = str_replace("{{title}}", ":title", $query);
+		// $query = str_replace("{{textMessage}}", ":text_message", $query);
+		// $query = str_replace("{{client_date}}", ":client_date", $query);
+		// $query = str_replace("{{server_date}}", ":server_date", $query);
+		// $query = str_replace("{{ip}}", ":ip", $query);
+		// $st = $connection->prepare($query);
+		
+		// $params = array();
+		// $params[":author_name"] = $authorName;
+		// $params[":title"] = $title;
+		// $params[":text_message"] = $textMessage;
+		// $params[":client_date"] = $clientDate;
+		// $params[":server_date"] = $serverDate;
+		// $params[":ip"] = $ip;
+// echo "<pre>";
+// print_r($params);
+// echo "</pre>";		
+		// $st->execute($params);
+		try{
+			$connection->query( $query );
+			echo "record was inserted...";
+		} catch( PDOException $exception ){
+			print_r($connection->errorInfo(), true);
+			echo $exception->getMessage();
+			exit();
+		}
+		
 	}
 		
 }//end saveMessage()	
@@ -193,9 +323,21 @@ function getMessages(){
 	global $_vars;
 
 	$query = $_vars["sql"]["getMessages"];
-	$messages = getDataObject( $query, $_vars["link"] );
+	
+	if($_vars["useMySQL"] == 1){
+		$messages = getDataObject( $query, $_vars["link"] );
+	}
+	if($_vars["usePDO"] == 1){
+		$connection = $_vars["link"];
+		$result  = $connection->query( $query ) or die( print_r($connection->errorInfo(), true) );
+		$messages  = $result->fetchAll( PDO::FETCH_ASSOC );
+// echo count($messages);	
+// echo "<pre>";	
+// print_r($messages);
+// echo "</pre>";	
+	}
+	
 	if( count($messages) > 0 ){
-		
 			if ( function_exists("json_encode") ){
 				//PHP 5 >= 5.2.0
 				$json = json_encode($messages);
@@ -207,7 +349,6 @@ function getMessages(){
 //https://gist.github.com/jorgeatorres/1239453
 echo "error, not use function json_encode(). incorrect PHP version - ".$_vars["config"]["phpversion"].", need PHP >= 5.2.0";
 			}
-		
 	}
 	
 }//end getMessages()	
@@ -218,13 +359,28 @@ function deleteMessage( $id ){
 	$query = $_vars["sql"]["deleteMessage"];
 	$query = str_replace("{{id}}", $id, $query);
 //echo $query;
-	if (mysql_query($query, $_vars["link"]) ) {
-		//to JSON!!!!
-		echo "record $id was deleted....<br>";
-	} else {
-		echo "error DELETE: " . mysql_error() . "<br>";
-		echo "SQL: " . $query . "<br>";
-		return false;
+
+	if($_vars["useMySQL"] == 1){
+		if (mysql_query($query, $_vars["link"]) ) {
+			//to JSON!!!!
+			echo "record $id was deleted...";
+		} else {
+			echo "error DELETE: " . mysql_error() . "<br>";
+			echo "SQL: " . $query;
+			exit();
+		}
+	}
+	
+	if($_vars["usePDO"] == 1){
+		$connection = $_vars["link"];
+		try{
+			$connection->query( $query );
+			echo "record $id was deleted...";
+		} catch( PDOException $exception ){
+			print_r($connection->errorInfo(), true);
+			echo $exception->getMessage();
+			exit();
+		}
 	}
 	
 }//end getMessages()	
