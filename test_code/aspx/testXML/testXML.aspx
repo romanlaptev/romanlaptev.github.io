9e690a6e5d037542d7c08aef6b00d592593d090b
romanlaptev <roman@vbox1>
2017-08-30 15:22:12 +0900

    update notes

---
+++ test_code/aspx/testXML/testXML.aspx
@@ -0,0 +1,5 @@
+<%@ Page 
+Language="C#" 
+CodeFile="testXML.aspx.cs" 
+Inherits="myspace.Default" 
+Debug="true"%>
---
+++ test_code/aspx/testXML/testXML.aspx.cs
@@ -0,0 +1,37 @@
+﻿//https://support.microsoft.com/ru-ru/help/307548/how-to-read-xml-from-a-file-by-using-visual-c
+using System;
+using System.Xml; 
+//using System.IO;
+
+namespace myspace
+{
+    public partial class Default : System.Web.UI.Page
+    {
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Response.Write("<h1>test XML</h1>");
+			
+			string filepath = Server.MapPath( "menu.xml");
+			XmlTextReader reader = new XmlTextReader (filepath);
+			while (reader.Read())  
+			{
+				switch (reader.NodeType)  
+				{
+					case XmlNodeType.Element: // Узел является элементом.
+						Response.Write("<" + reader.Name);
+						Response.Write(">");
+						break;
+			  case XmlNodeType.Text: // Вывести текст в каждом элементе.
+						Response.Write (reader.Value);
+						break;
+			  case XmlNodeType. EndElement: // Вывести конец элемента.
+						Response.Write("</" + reader.Name);
+						Response.Write(">");
+						break;
+				}
+			}			
+
+		}//end Page_Load()
+		
+	}//end class
+}//end namespace
