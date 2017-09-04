a3e9d50a757b06e76b8e7d9f524c8cec70704aa7
dev_user <dev_user>
2017-09-04 10:36:43 +0600

    update_notes

--- projects/notes/api/notes_mssql.aspx.cs
+++ projects/notes/api/notes_mssql.aspx.cs
@@ -7,6 +7,19 @@ using System.Data.SqlClient;
 
 public partial class _Default : System.Web.UI.Page
 {
+	static string dbHost = ".\\SQLEXPRESS2005";
+	static string dbName = "site";
+	static string dbUser = "sa";
+	static string dbPassword = "assa";
+	
+	static string exportFilename = "notes.xml";
+	static string uploadPath = "upload";
+	static string tableName = "user";//"notes";
+	static string queryGetNotes = "SELECT * FROM \""+tableName+"\"";
+
+	static string _connectionString = "";
+	static SqlConnection db_connection;
+	
 	protected void Page_Load(object sender, EventArgs e)
 	{
 		// foreach ( string x in Request.ServerVariables )
@@ -15,47 +28,23 @@ public partial class _Default : System.Web.UI.Page
 			// Response.Write ( "<br>"); 
 		// }
 		
-		// foreach ( string x in Request.Params )
-		// {
-			// Response.Write ( "<b>"+x + "</b> = " + Request.Params[x]); 
-			// Response.Write ( "<br>"); 
-		// }
-		
-		Response.Write("<h1>test connect to MSSQL server</h1>");
-		
-		string dbName = "site";
-		string _connectionString = "";
-		_connectionString = "Data Source=.\\SQLEXPRESS2005; Initial Catalog="+	dbName+"; Integrated Security=False;User Id=sa;Password=assa;";
-		SqlConnection db_connection = new SqlConnection( _connectionString );
+		_connectionString = "Data Source="+dbHost+"; ";
+		_connectionString += "Initial Catalog="+dbName+"; ";
+		_connectionString += "Integrated Security=False; ";
+		_connectionString += "User Id="+dbUser+";";
+		_connectionString += "Password="+dbPassword+";";
 
-
+		db_connection = new SqlConnection( _connectionString );
 		//int result = 0;
 		try
 		{
 			db_connection.Open();
-			
-			//SqlCommand command = new SqlCommand(queryString, connection);
-			//command.Connection.Open();
-			//command.ExecuteNonQuery();		
-			
-			SqlCommand myCommand;
-			SqlDataReader myDataReader;
-			
-			myCommand = new SqlCommand ("SELECT * FROM \"user\"", db_connection);
-			//result = myCommand.ExecuteNonQuery();
-			
-			myDataReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
-			while (myDataReader.Read())
-			{
-				Response.Write(myDataReader["user_login"].ToString());
-				Response.Write("<br>");
-			}
-			myDataReader.Close();
-			
+			//_testQuery();
+			_testRequestParams();
 		}
 		catch (Exception ex)
 		{
-			Response.Write("<h3>connect error.... </h3>");
+			Response.Write("connect error....");
 			Response.Write("connection string:" + _connectionString);
 		}
 		finally
@@ -64,4 +53,84 @@ public partial class _Default : System.Web.UI.Page
 		}	
 		
 	}//end Page_Load()
+	
+	protected void _testQuery()
+	{
+		SqlCommand myCommand;
+		SqlDataReader myDataReader;
+		
+		myCommand = new SqlCommand ( queryGetNotes, db_connection );
+		//result = myCommand.ExecuteNonQuery();
+		
+		myDataReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+		while (myDataReader.Read())
+		{
+			Response.Write(myDataReader["user_login"].ToString());
+			Response.Write("<br>");
+		}
+		myDataReader.Close();
+	}//end _testQuery()
+
+	protected void _testRequestParams()
+	{
+		// foreach ( string x in Request.Params )//GET
+		// {
+			// Response.Write ( "<b>"+x + "</b> = " + Request.Params[x]); 
+			// Response.Write ( "<br>"); 
+		// }
+		// foreach ( string x in Request.Form )//POST
+		// {
+			// Response.Write ( "<b>Request.Form["+x + "]</b> = " + Request.Form[x]); 
+			// Response.Write ( "<br>"); 
+		// }
+	
+		if( Request.QueryString["action"] == null )
+		{
+			Response.Write("error, undefined var 'action' ");
+			return;
+		}
+		string action = Request.QueryString["action"];
+		if( action.Length == 0 )
+		{
+			Response.Write("error, empty var 'action'");
+			return;
+		}
+		
+		switch (action)
+		{
+			case "save_notes":
+				//Console.WriteLine("Case 1");
+				break;
+				
+			case "get_notes":
+Response.Write ( action ); 
+Response.Write ( "<br>"); 
+					_testQuery();
+				break;
+				
+			case "delete_note":
+				break;
+				
+			case "clear_notes":
+				break;
+				
+			case "remove_table":
+				break;
+				
+			case "export_notes":
+				break;
+				
+			case "upload":
+				break;
+
+			case "import_notes":
+				break;
+				
+		  default:
+				//Console.WriteLine("Default case");
+				break;
+		}		
+		
+	}//end _testRequestParams()
+
 }//end class	
