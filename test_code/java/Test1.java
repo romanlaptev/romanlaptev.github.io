8f254a7f0e1771d20549e2b4c8fd68a118ca38b9
dev_user <roman-laptev@yandex.ru>
2017-09-25 10:20:40 +0600

    add java test

---
+++ test_code/java/Test1.java
@@ -0,0 +1,26 @@
+/*
+Compile: 
+D:\documents\laptev\www\test\java_src>javac Test1.java
+
+Run:
+D:\documents\laptev\www\test\java_src>java Test1 (no extension .class !!!!)
+Test1
+Test1
+Test1
+Test1
+Test1
+Test1
+Test1
+Test1
+Test1
+Test1
+*/
+class Test1{
+	public static void main (String args[]){
+		
+		for( int n = 0; n < 10; n++){
+			System.out.println("Test1");
+		}
+		
+	}
+}
