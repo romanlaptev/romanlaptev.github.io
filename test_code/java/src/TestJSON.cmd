96fd44b563b0165d2fc887b2a0209a381d4e557e
dev_user <roman-laptev@yandex.ru>
2017-10-04 14:24:29 +0600

    add test xml, java

---
+++ test_code/java/src/TestJSON.cmd
@@ -0,0 +1,2 @@
+javac TestJSON.java -classpath ../lib/gson-2.6.2.jar -d c:\temp\
+java -cp .;../lib/gson-2.6.2.jar;c:\temp TestJSON
--- test_code/java/src/TestJSON.java
+++ test_code/java/src/TestJSON.java
@@ -3,29 +3,59 @@
 //https://www.mkyong.com/java/how-to-convert-java-object-to-from-json-jackson/
 
 //package mypackage.xml;
-import javax.json.*;
+//import javax.json.*;
+
+// import org.codehaus.jackson.JsonGenerationException;
+// import org.codehaus.jackson.map.JsonMappingException;
+// import org.codehaus.jackson.map.ObjectMapper;
+
+//import java.util.ArrayList;
+//import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+//import com.google.gson.JsonElement;
+//import com.google.gson.JsonObject;
+//import com.google.gson.JsonParser;
+
+//import com.google.gson.GsonBuilder;
 
 class TestJSON{
 	
 	public static void main (String args[]){
 
-		JsonObject value = Json.createObjectBuilder()
-		 .add("firstName", "John")
-		 .add("lastName", "Smith")
-		 .add("age", 25)
-		 .add("address", Json.createObjectBuilder()
-			 .add("streetAddress", "21 2nd Street")
-			 .add("city", "New York")
-			 .add("state", "NY")
-			 .add("postalCode", "10021"))
-		 .add("phoneNumber", Json.createArrayBuilder()
-			 .add(Json.createObjectBuilder()
-				 .add("type", "home")
-				 .add("number", "212 555-1234"))
-			 .add(Json.createObjectBuilder()
-				 .add("type", "fax")
-				 .add("number", "646 555-4567")))
-		 .build();
+		Gson gson = new Gson();
+		
+		Map<String, String> dBrecord = new HashMap<String, String>();
+		dBrecord.put("id", "1");
+		dBrecord.put("author", "anonymous");
+		dBrecord.put("title", "test1");
+		dBrecord.put("text_message", "test1111");
+		dBrecord.put("client_date", "2017-10-02 16:04:58");
+		dBrecord.put("server_date", "2017-10-02 09:08:40");
+		dBrecord.put("ip", "37.193.108.45");
+		
+		String jsonStr = gson.toJson(dBrecord);
+		System.out.println( jsonStr);
+		
+		// JsonObject value = Json.createObjectBuilder()
+		 // .add("firstName", "John")
+		 // .add("lastName", "Smith")
+		 // .add("age", 25)
+		 // .add("address", Json.createObjectBuilder()
+			 // .add("streetAddress", "21 2nd Street")
+			 // .add("city", "New York")
+			 // .add("state", "NY")
+			 // .add("postalCode", "10021"))
+		 // .add("phoneNumber", Json.createArrayBuilder()
+			 // .add(Json.createObjectBuilder()
+				 // .add("type", "home")
+				 // .add("number", "212 555-1234"))
+			 // .add(Json.createObjectBuilder()
+				 // .add("type", "fax")
+				 // .add("number", "646 555-4567")))
+		 // .build();
  
 // JsonObject can be written to JSON as follows:
 
