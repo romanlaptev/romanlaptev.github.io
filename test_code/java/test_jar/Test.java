ad8fdf31e621d69450637710d3e56754e667365c
dev_user <roman-laptev@yandex.ru>
2017-10-18 15:04:04 +0600

    update java tests

---
+++ test_code/java/test_jar/Test.java
@@ -0,0 +1,10 @@
+import inc.TestClass;
+
+public class Test
+{
+	public static void main( String[] args){
+		TestClass testObj = new TestClass();
+		testObj.About();
+	}
+	
+}//end class
---
+++ test_code/java/test_jar/inc/TestClass.java
@@ -0,0 +1,10 @@
+package inc;
+
+public class TestClass
+{
+	
+	public void About() {
+		System.out.println("TestClass.About...");
+	}
+	
+}//end class
