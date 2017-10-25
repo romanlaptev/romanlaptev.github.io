efa7bb2b9ca3f3937fd5f5be6e7cd01331b6ad57
eee-pc user <roman-laptev@yandex.ru>
2017-10-25 20:46:15 +0600

    update java tests

--- test_code/java/src/TestVector.java
+++ test_code/java/src/TestVector.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+//java TestVector a b c d e f
 class TestVector{
 	public static void main (String args[]){
 	
@@ -8,9 +8,15 @@ class TestVector{
 		for( int n = 0; n < args.length; n++){
 			v.add( args[n] );
 		}
+		
+		v.set(2, "replace element with index 2!!!");
+		v.remove("a");
+		
 		for( int n = 0; n < v.size(); n++){
 			System.out.println( v.get(n) );
 		}
+
+		System.out.println( "Index of element: " + v.indexOf("b") );
 		
 	}
 }//end class
