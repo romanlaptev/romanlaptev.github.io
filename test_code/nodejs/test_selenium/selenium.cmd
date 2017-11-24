fcd86830bcee9fc428f26d8d5b0b62b18598e239
romanlaptev <roman@vbox1>
2017-11-24 17:16:54 +0800

    add nodejs test

---
+++ test_code/nodejs/test_selenium/selenium.cmd
@@ -0,0 +1,11 @@
+rem http://itmultimedia.ru/avtomatizaciya-testirovaniya-ustanovka-i-nastrojka-selenium-server-i-selenium-webdriver-na-ubuntu-16-04-lts/
+
+rem npm install selenium-webdriver
+
+REM npm install selenium-standalone@latest -g
+REM selenium-standalone install 
+
+rem selenium-standalone start 
+
+rem Run test
+node test_selenium.js 
