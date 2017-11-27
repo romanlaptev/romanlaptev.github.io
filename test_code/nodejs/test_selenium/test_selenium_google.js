b080d3a690a95d1c3439741f301d5e62ebfdbfed
romanlaptev <roman@vbox1>
2017-11-27 09:41:41 +0800

    update translates

---
+++ test_code/nodejs/test_selenium/test_selenium_google.js
@@ -0,0 +1,23 @@
+var webdriver = require("selenium-webdriver");
+
+// Input capabilities
+var capabilities = {
+"browserName" : "chrome",
+}
+
+var driver = new webdriver.Builder().
+usingServer("http://localhost:4444/wd/hub").
+withCapabilities(capabilities).
+build();
+
+driver.get("http://www.google.com");
+driver.findElement(webdriver.By.name("q")).sendKeys("video testing");
+//driver.findElement(webdriver.By.name("q")).sendKeys("video testing\n");
+//driver.findElement(webdriver.By.name("btnG")).click();
+driver.findElement( webdriver.By.id("lst-ib") ).sendKeys( webdriver.Key.ENTER );
+
+driver.getTitle().then(function(title) {
+console.log(title);
+});
+
+//driver.quit();
