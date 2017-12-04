5360984cb0004c84385e04d4abfa57b6dd34d8aa
romanlaptev <roman@vbox1>
2017-12-04 12:13:55 +0800

    update test Vue

--- test_code/js/test_Vue/js/custom.js
+++ test_code/js/test_Vue/js/custom.js
@@ -1,3 +1,5 @@
+var app3, app4;
+
 //console.log("Vue:" + typeof Vue);
 var support = true;
 if( typeof Vue === "undefined"){
@@ -10,9 +12,24 @@ window.onload=function(){
 		testVue();
 	} else {
 		var $ver = getById("version");
-		var msg = "This browser does not support the framework Vue!!!";
+		var msg = "This browser does not support working with the framework Vue!!!";
 		$ver.innerHTML = "<p class='alert-danger'>"+msg+"</p>";
 	}
+
+	var $btn_visibility = getById("btn-visibility");
+	$btn_visibility.onclick = function(e){
+console.log(app3);
+		if( app3.seen ){
+			app3.seen = false;
+		} else {
+			app3.seen = true;
+		}
+	}//end event
+	
+	var $btn_push = getById("btn-push");
+	$btn_push.onclick = function(e){
+		app4.todos.push({"text" : "new task"});
+	}//end event
 	
 }//end load
 
@@ -41,7 +58,49 @@ console.log(event);
 	  data: {
 		message: 'Вы загрузили эту страницу в: ' + new Date().toLocaleString()
 	  }
-	})
+	});
+	
+	//https://ru.vuejs.org/v2/guide/index.html
+	app3 = new Vue({
+		el: '#app-3',
+		data: {
+			seen: true
+		}
+	});
+
+	app4 = new Vue({
+		el: "#app-4",
+		data: {
+			todos : [
+				{text: "task1"},
+				{text: "task2"},
+				{text: "task3"}
+			]
+		}
+	});
+	
+	var app5 = new Vue({
+		el:"#app-5",
+		data: {
+			message: "text1"
+		},
+		methods: {
+			method1: function( e ){
+//console.log( arguments );
+//console.log( this.message );
+				this.message = "Coordinates:  x = " + e.clientX +", y = "+ e.clientY;
+			}
+		}
+		
+	});
+	
+	
+	var app6 = new Vue({
+		el: "#app-6",
+		data : {
+			name: "Jack"
+		}
+	});
 	
 }//end testVue()
 
