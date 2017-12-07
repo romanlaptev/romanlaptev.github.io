c18f93563cab85c57de13ba92691521fb5bcc56f
romanlaptev <roman@vbox1>
2017-12-07 18:05:26 +0800

    update test ReactJS

--- test_code/js/test_React/js/custom.js
+++ test_code/js/test_React/js/custom.js
@@ -1,13 +1,15 @@
 ﻿   // рендеринг элемента
     ReactDOM.render(
-        <h1>Hello React, once more</h1>,  // элемент, который мы хотим создать
-        document.getElementById("root2")    // где мы этот элемент хотим создать
-    )
+		<h1>Hello React, once more...</h1>,
+		document.getElementById("root2"), function(){
+console.log( arguments );			
+		}
+    );
 
     ReactDOM.render(
         <h2>2 + 2 = {2 + 2}</h2>,
         document.getElementById("ex2")
-    )
+    );
 	
     const user = {
         id : 5,
@@ -40,4 +42,84 @@
     ReactDOM.render(
         <Hello></Hello>,
         document.getElementById("ex4")
-    )
+    );
+	
+	//https://abraxabra.ru/react.js/bystryy-start/react-js-otrisovka-elementov/
+	function tick() {
+		const element = (<h2>{new Date().toLocaleTimeString()}</h2>);
+		ReactDOM.render(
+			element,
+			document.getElementById("ex-timer")
+		);
+	};
+	setInterval(tick, 1000);
+
+	//https://abraxabra.ru/react.js/bystryy-start/react-js-komponenty-i-svoystva/
+	function Component (props) {
+		return <p>Component, {props.name}</p>;
+	}
+	const element = <Component name="component 1" />;
+	ReactDOM.render(
+		element,
+		document.getElementById("ex-component")
+	);
+
+	class Component2 extends React.Component {
+		render() {
+			return <h1>Hello, {this.props.name}</h1>;
+		}
+	};
+	const element2 = <Component2 name="all !!!" />;
+	ReactDOM.render(
+		element2,
+		document.getElementById("ex-component2")
+	);
+	
+	
+/*
+	//https://habrahabr.ru/post/229629/
+	var MenuExample = React.createClass({
+
+		getInitialState: function(){
+			return { focused: 0 };
+		},
+
+		clicked: function(index){
+			// Обработчик клика обновит состояние
+			// изменив индекс на сфокусированный элемент меню
+			this.setState({focused: index});
+		},
+
+		render: function() {
+			// Здесь мы читаем свойство items, которое было передано
+			// атрибутом, при создании компонента
+			var self = this;
+			// Метод map пройдется по массиву элементов меню,
+			// и возвратит массив с <li> элементами.
+			return (
+				<div>
+					<ul>{ this.props.items.map(function(m, index){
+			
+						var style = '';
+			
+						if(self.state.focused == index){
+							style = 'focused';
+						}
+			
+						// Обратите внимание на использование метода bind(). Он делает
+						// index доступным в функции clicked:
+						return <li className={style} onClick={self.clicked.bind(self, index)}>{m}</li>;
+					}) }
+							
+					</ul>
+					<p>Selected: {this.props.items[this.state.focused]}</p>
+				</div>
+			);
+		}//end render()
+	});
+	
+	React.renderComponent(
+		<MenuExample items={ ['Home', 'Services', 'About', 'Contact us'] } />,
+		document.body
+	);	
+*/
