68c8a399250c518bafda2d6b41607e43146bbf9c
romanlaptev <roman@vbox1>
2018-02-22 16:50:57 +0800

    test express

--- test_code/nodejs/test_express_server/helloapp.js
+++ test_code/nodejs/test_express_server/helloapp.js
@@ -18,39 +18,7 @@ app.get("/", function( req, res ){
 console.log('GET request to the homepage');
 //console.log(req);
 //console.log(res);
-	var html = "<!DOCTYPE html>\r\n<html>\r\n";
-
-	html += "<head>\r\n";
-	html += "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>\r\n";
-	html += "<link rel='stylesheet' href='/css/style.css'>\r\n";
-	html += "<link rel='stylesheet' href='/style_public.css'>\r\n";
-	
-	html += "</head>\r\n";
-	
-	html += "<body>\r\n";
-	html += "<div class='container'>\r\n";
-	
-	html += "<h1>Hello Express!!!</h1>\r\n";
-	
-	html += "<pre>\r\n";
-	html += "http://expressjs.com/ru/guide/routing.html\r\n";
-	html += "</pre>\r\n";
-	
-	html += "<ul class='list-unstyled'>\r\n";
-	html += "<li><a class='btn btn-large btn-primary' href='/test'>load local text file</a></li>\r\n";
-	html += "</ul>\r\n";
-	
-	html += "<h3>test post request</h3>\r\n";
-	html += "<form method='POST' name='form1' action=''>";
-	html += "<input type='text' name='field1'>";
-	html += "<input type='submit'>";
-	html += "</form>\r\n";
-	
-	html += "</div>\r\n";
-	html += "</body>\r\n";
-	html += "</html>";
-	
-	res.send( html );
+	//res.send( "Hello!" );
 });
 
 // POST method route
@@ -58,13 +26,13 @@ app.post('/', function (req, res) {
 console.log('POST request to the homepage');
 //console.log(req);
 //console.log(res);
-res.end();
+console.log(req.body);
 });
 
-app.get("/css/style.css", function(req, res){
-console.log("request to the /css/style.css");
+//app.get("/css/style.css", function(req, res){
+//console.log("request to the /css/style.css");
 //res.download("/documents/laptev/www/test/nodejs/test_express/css/style.css");
-res.end();
+//res.end();
 
 /*
 res.download() 	Приглашение загрузки файла.
@@ -77,7 +45,7 @@ res.send() 	Отправка ответа различных типов.
 res.sendFile 	Отправка файла в виде потока октетов.
 res.sendStatus() 	Установка кода состояния ответа и отправка представления в виде строки в качестве тела ответа.
 */
-});
+//});
 
 app.get("/test", function(req, res){
 console.log("request to the " + req.originalUrl);
