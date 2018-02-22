973527911d348ab23189c58f9330ce9bbed25489
romanlaptev <roman@vbox1>
2018-02-22 12:43:04 +0800

    test express

--- test_code/nodejs/test_express_server/helloapp.js
+++ test_code/nodejs/test_express_server/helloapp.js
@@ -1,12 +1,86 @@
 var express = require("express");
+//console.log(express.request.ip);
+/*
+ is: [Function: is],
+ protocol: [Getter],
+ secure: [Getter],
+ ip: [Getter],
+ ips: [Getter],
+ subdomains: [Getter],
+ path: [Getter],
+ hostname: [Getter],
+ host: [Getter],
+ fresh: [Getter],
+ stale: [Getter],
+ xhr: [Getter] }
+*/
+
 var app = express();
 
+app.all("/", function (req, res, next) {
+console.log("Request...");
+	next(); // pass control to the next handler
+});
+
+// GET method route
 app.get("/", function( req, res ){
-	var html = "<h1>Hello Express!!!</h1>";
-	html += "22222222222222";
+console.log('GET request to the homepage');
+//console.log(req);
+//console.log(res);
+	var html = "<!DOCTYPE html>\r\n<html>\r\n";
+
+	html += "<head>\r\n";
+	html += "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>\r\n";
+	html += "<link rel='stylesheet' href='/css/style.css'>\r\n";
+	
+	html += "</head>\r\n";
+	
+	html += "<body>\r\n";
+	html += "<div class='container'>\r\n";
+	
+	html += "<h1>Hello Express!!!</h1>\r\n";
+	
+	html += "<pre>\r\n";
+	html += "http://expressjs.com/ru/guide/routing.html\r\n";
+	html += "</pre>\r\n";
+	
+	html += "<h3>test post request</h3>\r\n";
+	html += "<form method='POST' name='form1' action=''>";
+	html += "<input type='text' name='field1'>";
+	html += "<input type='submit'>";
+	html += "</form>\r\n";
+	
+	html += "</div>\r\n";
+	html += "</body>\r\n";
+	html += "</html>";
+	
 	res.send( html );
 });
 
+// POST method route
+app.post('/', function (req, res) {
+console.log('POST request to the homepage');
+//console.log(req);
+//console.log(res);
+});
+
+app.get("/css/style.css", function(req, res){
+console.log("request to the /css/style.css");
+res.download("/documents/laptev/www/test/nodejs/test_express/css/style.css");
+/*
+res.download() 	Приглашение загрузки файла.
+res.end() 	Завершение процесса ответа.
+res.json() 	Отправка ответа JSON.
+res.jsonp() 	Отправка ответа JSON с поддержкой JSONP.
+res.redirect() 	Перенаправление ответа.
+res.render() 	Вывод шаблона представления.
+res.send() 	Отправка ответа различных типов.
+res.sendFile 	Отправка файла в виде потока октетов.
+res.sendStatus() 	Установка кода состояния ответа и отправка представления в виде строки в качестве тела ответа.
+*/
+});
+
+
 app.listen(3000, function(){
-	console.log('Example app listening on port 3000!');
+console.log('Example app listening on port 3000!');
 });
