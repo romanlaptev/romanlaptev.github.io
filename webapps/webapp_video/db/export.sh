5a323c917388a8710bfe650b658eee1a70d6a98e
romanlaptev <roman@vbox1>
2018-03-21 12:43:23 +0800

    webapp_video, dump

--- webapps/webapp_video/db/export.sh
+++ webapps/webapp_video/db/export.sh
@@ -1,1 +1,32 @@
-sqlite3 video.sqlite .dump > video.sql
+#sqlite3 video.sqlite .dump > video.sql
+#!/bin/bash
+
+# sqlite3 video.sqlite <<!
+# .headers on
+# .mode csv
+# .output out.csv
+# select * from node;
+# !
+
+dbName="video.sqlite"
+
+tableName="node"
+fields="nid, vid, type, title, created"
+echo "- export table ${tableName}"
+sqlite3 ${dbName} <<!
+.headers on
+.mode csv
+.output ${tableName}.csv
+select ${fields} from ${tableName};
+!
+
+tableName="node_type"
+fields="type, name, orig_type"
+echo "- export table ${tableName}"
+sqlite3 ${dbName} <<!
+.headers on
+.mode csv
+.output ${tableName}.csv
+select ${fields} from ${tableName};
+!
+
