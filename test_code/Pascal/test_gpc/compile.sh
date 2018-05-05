3f9cdb32e04c2b2bd42b92b4e167f637aa933ec9
romanlaptev <roman-laptev@yandex.ru>
2018-05-05 22:28:47 +0700

    update test_code, pascal

---
+++ test_code/Pascal/test_gpc/compile.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+gpc test_gpc.pas -o test_gpc -v
---
+++ test_code/Pascal/test_gpc/test_gpc.pas
@@ -0,0 +1,10 @@
+program test_gpc;
+var
+ n :integer;
+
+begin
+	for n:=1 to 10 do
+	begin
+		writeln('Test gpc, GNU project Pascal Compiler');
+	end;
+end.
