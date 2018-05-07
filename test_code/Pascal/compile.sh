e192a7af6bc41bdbc573255f35de9e16a69175c7
romanlaptev <roman@vbox1>
2018-05-07 09:24:17 +0800

    update test_code, pascal

---
+++ test_code/Pascal/compile.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+#gpc test_gpc.pas -o test_gpc -v
+fpc test_fpc.pas -otestFPC -v
---
+++ test_code/Pascal/test_fpc.pas
@@ -0,0 +1,10 @@
+program test_fpc;
+var
+ n :integer;
+
+begin
+	for n:=1 to 10 do
+	begin
+		writeln('Test fpc, Free Pascal Compiler');
+	end;
+end.
