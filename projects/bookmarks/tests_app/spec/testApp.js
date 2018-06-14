535b70cbc45a43ffb0b0c55fd468c0548814d211
romanlaptev <roman@vbox5>
2018-06-14 12:58:45 +0800

    update bookmarks webapp

--- projects/bookmarks/tests_app/spec/testApp.js
+++ projects/bookmarks/tests_app/spec/testApp.js
@@ -1,191 +1,6 @@
 //jasmine.getJSONFixtures().fixturesPath = __confFixturesPathJSON;
 //jasmine.getFixtures().fixturesPath = "html";
 //console.log( jasmine );
-//console.log( webApp.draw );
-
-var _getRecords_ = webApp.iDBmodule.getRecords;
-webApp.iDBmodule.getRecords = function( opt ){
-console.log("run fake getRecords()");
-console.log(opt);
-
-	var storeName = opt["storeName"];
-	var data = [];
-	switch( storeName ){
-		case "node":
-var record = {
-changed: "1491727695",
-//checkResult: Array [ true ]
-comment: "0",
-created: "1336651468",
-language: "ru",
-moderate: "0",
-nid: "20",
-promote: "1",
-status: "1",
-sticky: "0",
-title: "Ballet Mistress",
-tnid: "0",
-translate: "0",
-type: "photogallery_image",
-uid: "1",
-vid: "20"
-};
-			data.push(record);
-//console.log( data );
-			if( typeof opt["callback"] == "function"){
-				opt["callback"]( data );
-			}
-		break;
-		
-		case "node_revisions":
-var record = {
-body: "",
-//checkResult: Array [ true ]
-format: "1",
-log: "",
-nid: "20",
-teaser: "",
-timestamp: "1491727695",
-title: "Ballet Mistress",
-uid: "1",
-vid: "20"
-};
-			data.push(record);
-//console.log( data );
-			if( typeof opt["callback"] == "function"){
-				opt["callback"]( data );
-			}
-		break;
-		
-		case "content_node_field_instance":
-var record = {
-	field_name: "field_author",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_num_page",
-	type_name: "photogallery_image"
- };
-data.push(record);
-			
-record = {
-	field_name: "field_img1_gallery",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_big_img",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_preview_img",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_title",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_test",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_content_location",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_original_img",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_info",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_preview_img_preset",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_termin_image",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_termin_images_location",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_filename",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-record = {
-	field_name: "field_zoom_img",
-	type_name: "photogallery_image"
- };
-data.push(record);
-
-//console.log( data );
-			if( typeof opt["callback"] == "function"){
-				opt["callback"]( data );
-			}
-		break;
-		
-		case "content_type_photogallery_image":
-var record = {
-field_author_value: "Майкл Паркес",
-field_big_img_value: "/site-content/albums/gallery_images/imagecache/w1024/gallery_images",
-field_create_date_value: "1994",
-field_filename_value: "ballet_mistress.jpg",
-field_img1_gallery_data: "NULL",
-field_img1_gallery_fid: "NULL",
-field_img1_gallery_list: "NULL",
-field_info_format: "NULL",
-field_info_value: "NULL",
-field_num_page_value: "1994",
-field_original_img_value: "/site-content/albums/gallery_images",
-field_preview_img_preset_value: "preview_gallery_img_portrait",
-field_preview_img_value: "/site-content/albums/gallery_images/imagecache/preview_gallery_img/gallery_images",
-field_test_value: "NULL",
-field_title_value: "Ballet Mistress, 1994, Майкл Паркес",
-field_zoom_img_value: "NULL",
-nid: "20",
-vid: "20"
-};
-			data.push(record);
-			if( typeof opt["callback"] == "function"){
-				opt["callback"]( data );
-			}
-		break;
-		
-	}//end switch()
-};//end fake getRecords()
-		
-//console.log( webApp.iDBmodule.getRecords.toString() );
-//console.log( _getRecords_.toString() );
 
 /*
 describe("test webapp", function(){
@@ -213,22 +28,44 @@ console.log("afterEach ", arguments);
 describe("test application", function(){
 	
 	beforeAll(function(){
+console.log("beforeAll", arguments);
 		//loadFixtures("test.index.html");
 		// spyOn( webApp.iDBmodule, "getRecords" ).and.callFake( function(){
 // console.log("test method .getRecords(), fake call", arguments);
 		// });
 	});
-			
-	it("test 1", function(){
-		expect(true).toBe(true);
-	});//end it
+
+	beforeEach(function(){
+console.log("beforeEach", arguments);
+	});
 	
-//get children termins by parameters "vocName", "termName"
-//webApp.db.getBlockContent
-	//_getVocabularyByName
-	//_getTermByName
-	//_getChildTerms
+	afterEach(function(){
+console.log("afterEach", arguments);
+	});
+	
+	it("test1, creating webApp object", function(){
+		var res = typeof webApp === "object";
+//console.log( typeof webApp, res );	
+		expect( res ).toBe(true);
+	});//end it
 
-//_formQueryObj() to replace getBlockContent() !!!!!!
+	it("test2, checking webApp.vars objects", function(){
+		var res = typeof webApp.vars === "object";
+console.log( typeof webApp.vars, res );	
+		expect( res ).toBe(true);
+	});//end it
 
+	// it("test2, checking load DOM objects", function(){
+		
+		// "pageContainer" : getById("content-column"),
+		// "insertContainer" : getById("insert-json"),
+		// "btnParse" : getById("btn-parse"),
+		// "wait" : getById("wait"),
+		// "waitWindow" : getById("win1"),
+		
+		// // var res = typeof webApp === "object";
+// // console.log( typeof webApp, res );	
+		// // expect( res ).toBe(true);
+	// });//end it
+	
 });//end describe
