9b5abf502f3fc0e4a87577ba7278ed93d5bb3455
romanlaptev <roman@vbox5>
2018-06-27 17:01:13 +0800

    update react tests

--- test_code/js/test_React/test_React-bootstrap/src/index.js
+++ test_code/js/test_React/test_React-bootstrap/src/index.js
@@ -6,3 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+//-------------------------
+var ua = "<h2>"+navigator.userAgent+"</h2>";
+document.querySelector("#info").innerHTML += ua;
+document.querySelector("#version").innerHTML = React.version;
