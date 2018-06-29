31af9142ab581e5dba3a44ce918d4d209ef92593
romanlaptev <roman@vbox5>
2018-06-29 16:32:25 +0800

    update test React

---
+++ test_code/js/test_React/js/test4.js
@@ -0,0 +1,29 @@
+﻿// class Block extends React.Component {
+    // render() {
+        // return ( <h3>itProger</h3> ); 
+    // }
+// }
+
+//define component, functional variant
+var TestComponent = function(){
+console.log( "create TestComponent, args = ", arguments );			
+	return (
+<h1>TestComponent...</h1>	
+	);
+}//end TestComponent()
+
+// function TestComponent(){
+// console.log( "create TestComponent, args = ", arguments );			
+	// return <h1>TestComponent...</h1>
+// };
+
+console.log( TestComponent );	
+
+ReactDOM.render( 
+	<TestComponent />, 
+	document.querySelector("#root"), 
+	function(){
+console.log( "post render, ", arguments );			
+	}
+);
+
