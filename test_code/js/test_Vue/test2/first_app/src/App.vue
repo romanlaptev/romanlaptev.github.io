f1b64c3bfa88506960138fc4079d37a9862a26e2
romanlaptev <roman@vbox5>
2018-07-04 12:22:59 +0800

    update Vue tests

--- test_code/js/test_Vue/test2/first_app/src/App.vue
+++ test_code/js/test_Vue/test2/first_app/src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 
+		<img src="./assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/> 
+	-->
+	<div class="panel">
+		<Component1 msg="Hello Vue world !!!" />
+	</div>
+	
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
+/*
 export default {
   name: 'app',
   components: {
     HelloWorld
   }
 }
+*/
+
+import Component1 from './components/Component1.vue'
+export default {
+  name: 'app',
+  components: {
+	Component1
+  }
+}
+
 </script>
 
 <style>
+/*
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +43,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+*/
 </style>
--- test_code/js/test_Vue/test2/first_app/src/main.js
+++ test_code/js/test_Vue/test2/first_app/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//-------------------------
 console.log("Vue version:" + Vue.version);
+var ua = "<h2>"+navigator.userAgent+"</h2>";
+document.querySelector("#info").innerHTML += ua;
+document.querySelector("#version").innerHTML = Vue.version;
+//-------------------------
 
 Vue.config.productionTip = false
 
