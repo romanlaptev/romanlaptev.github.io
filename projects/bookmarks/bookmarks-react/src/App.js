8629b4216ccc2ed007327b2add685e9a8d0c2f94
romanlaptev <roman@vbox5>
2018-07-06 17:11:02 +0800

    update bookmarsk, add react

---
+++ projects/bookmarks/bookmarks-react/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+import Link from "./components/Link";
+console.log(Link);
+
+class App extends Component {
+  render() {
+    return (
+	<div className="App">
+	
+		<h1>Bookmarks</h1>
+
+		<ul>
+			<li><Link title={"Yandex"} url={"http://yandex.ru"} /></li>
+			<li><Link title={"Jest 23.3"} url={"https://jestjs.io/docs/ru/getting-started"} /></li>
+		</ul>
+
+	  </div>
+    );
+  }
+}
+
+export default App;
