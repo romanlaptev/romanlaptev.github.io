a496301b2a304c80831c9d631785828556187151
romanlaptev <roman@vbox5>
2018-08-08 11:50:30 +0800

    update nodejs test sqlite

--- test_code/nodejs/test_sqlite/test_sqlite_file.js
+++ test_code/nodejs/test_sqlite/test_sqlite_file.js
@@ -1,56 +1,77 @@
 //http://www.sqlitetutorial.net/sqlite-nodejs/connect/
+//http://www.sqlitetutorial.net/sqlite-nodejs/query/
 //npm install sqlite3 
 //node test_sqlite.js
 
 var fs = require("fs");
-var dbFile = "./db/testdrive.db";
-var xml = "<table>{{records}}</table>";
-var xmlRec = "<record>\
-<name>{{username}}</name>\
-<password>{{pwd}}</password>\
-</record>";
-var exportFile = "testdrive.xml";
+
+_vars = {
+	"dbFile" : "./db/testdrive.db",
+	
+	"xml" : "<table>\r\n\{{records}}\</table>",
+	"xmlRecTpl" : "\t<record>\r\n\
+\t\t<name>{{username}}</name>\r\n\t\t<password>{{pwd}}</password>\r\n\
+\t</record>\r\n",
+
+	"exportFile" : "testdrive.xml",
+	"sql" : "SELECT username, password as pwd FROM tbl_user"
+	
+};//end _vars{}
 
 const sqlite3 = require("sqlite3").verbose();
-let db = new sqlite3.Database( dbFile, sqlite3.OPEN_READONLY, (err) => {
+let db = new sqlite3.Database( _vars["dbFile"], sqlite3.OPEN_READONLY, (err) => {
 	if (err) {
 	console.error(err.message);
 	}
-	console.log('Connected to the database.');
+console.log('Connected to the database.');
 });
 
-
-db.serialize(() => {
-	
-	db.each("SELECT username, password as pwd FROM tbl_user", (err, row) => {
-		if (err) {
-console.error(err.message);
-		}
-//console.log( row.username + "\t" + row.pwd );
-		xmlRec = xmlRec
-		.replace("{{username}}", row.username)
-		.replace("{{pwd}}", row.pwd);
-	});
-	
+//Get data
+let params = [];
+db.all( _vars["sql"], params, (err, rows) => {
+	if (err) {
+		throw err;
+	}
+//console.log( "TEST1", rows );
+	_formXML( rows );
 });
-
-xml = xml.replace("{{records}}", xmlRec);
 
 db.close((err) => {
-if (err) {
-console.error(err.message);
-}
+	if (err) {
+	console.error(err.message);
+	}
 console.log("Close the database connection.");
 });
 
+//===================================
+function _formXML(rows){
+	
+	var xmlRecords = "";
+	rows.forEach( (row) => {
+//console.log( row.username + "\t" + row.pwd );
+		xmlRecords += _vars["xmlRecTpl"]
+		.replace("{{username}}", row.username)
+		.replace("{{pwd}}", row.pwd);
+	});
+//console.log( "TEST3", xmlRecords, xmlRecords.length );
 
-fs.writeFile( exportFile, xml, function(error){
-	if (error) {
-		throw error; 
-	}
+	_vars["xml"] = _vars["xml"].replace("{{records}}", xmlRecords);
+//console.log( _vars["xml"] );
+
+	_saveXML( _vars["exportFile"], _vars["xml"] );
+}//end _formXML()
+
+
+function _saveXML( filepath, xml){
+	
+	fs.writeFile( filepath, xml, function(error){
+		if (error) {
+			throw error; 
+		}
 console.log("Async write is over. Content file:");
 
-	var fileContent = fs.readFileSync( exportFile, "utf8");
+		var fileContent = fs.readFileSync( filepath, "utf8");
 console.log( fileContent ); 
-
-});
+	});
+	
+}//end _saveXML()
