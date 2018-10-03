82ce6a8a4a3a27f553f55a53313cd6e421509f20
romanlaptev <roman@vbox5>
2018-10-03 13:19:41 +0800

    update webapp lib

--- webapps/webapp_lib/js/webapp_lib.js
+++ webapps/webapp_lib/js/webapp_lib.js
@@ -52,7 +52,6 @@ var info = [];
 			}
 			
 //-----------------
-/*
 //console.log(typeof document.queryCommandSupported);
 try{
 //console.log( document.queryCommandSupported("copy") );
@@ -78,7 +77,6 @@ console.log( "- name: " + e.name );
 console.log( "- message: " + e.message );
 console.log( "- result: " + e.result );
 }
-*/
 //-----------------			
 
 			if ( config["use_localcache"] ) {
@@ -2704,15 +2702,14 @@ $(".b-content").height(_newHeight);
 				}
 				
 var directLink = "";
-//var btnCopyUrl = "";
-//var desc = "";
+var btnCopyUrl = "";
+var desc = "";
 //-------------
 if(cloudUrl.indexOf("mail.ru") !== -1 ){
 	desc = "Mail.ru cloud disk: ";
-	//directLink = "<div id='link-"+n+"'>" + url+"</div>";
+	directLink = "<div id='link-"+n+"'>" + url+"</div>";
 	
 //------------- add COPY LINK BUTTON
-/*
 	if(config["addCopyLink"]){
 		btnCopyUrl = "<button id='btn-copy-"+n+"' class='btn btn-primary btn-sm btn-copy-url' data-clipboard-action='copy' data-clipboard-target='#link-"+n+"'>Copy link to the clipboard</button>";
 		
@@ -2730,7 +2727,6 @@ if(cloudUrl.indexOf("mail.ru") !== -1 ){
 	console.log("error copy link", e);
 		});
 	}
-*/	
 }
 
 if(cloudUrl.indexOf("yandex") !== -1 ){
@@ -2740,9 +2736,9 @@ if(cloudUrl.indexOf("yandex") !== -1 ){
 				var html_url = node_tpl_url
 						.replace("{{link-title}}", link_title)
 						.replace(/{{url}}/g, url)
-						.replace(/{{description}}/g, desc);
-						//.replace(/{{direct-link}}/g, directLink)
-						//.replace(/{{btn-copy-url}}/g, btnCopyUrl);
+						.replace(/{{description}}/g, desc)
+						.replace(/{{direct-link}}/g, directLink)
+						.replace(/{{btn-copy-url}}/g, btnCopyUrl);
 
 				html += html_url;
 			}//next book file
