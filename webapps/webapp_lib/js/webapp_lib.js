83a3a587a68c437eac406afbb5e90ec8d393c7fb
romanlaptev <roman@vbox5>
2018-10-16 13:02:53 +0800

    update webapp lib

--- webapps/webapp_lib/js/webapp_lib.js
+++ webapps/webapp_lib/js/webapp_lib.js
@@ -2546,8 +2546,18 @@ console.log("_vars['book_child_pages'] is empty!!!", _vars["book_child_pages"].l
 			
 			if( typeof _vars["book_category"] !== "undefined" ) {
 				if( _vars["book_category"].length > 0 ) {
-					var html = book.view_book_category();
-					$("#block-book-category").html( html );
+					//var html = book.view_book_category();
+					//$("#block-book-category").html( html );
+					
+//--------------------- BLOCK #block-book-category
+					var opt = {
+						"locationID" : "block-book-category",
+						//"title" : "test block",
+						"templateID" : "tpl-block--book-category",
+						"content" : book.view_book_category() 
+					};
+					_buildBlock( opt );
+//---------------------
 					
 					//mark root links for breadcrumb navigation
 					$("#block-book-category .nav-click").addClass("root-link");			
@@ -2674,23 +2684,12 @@ console.log("error, not found _vars[book_category]");
 			$("#breadcrumb-tpl").html( html_breadcrumb );
 			
 //--------------------- BLOCK #sitename-block
-			
-	var opt = {
-		"locationID" : "sitename-block",
-		//"title" : "test block",
-		"templateID" : "tpl-block--sitename",//optional
-		//"contentTpl" : "tpl-menu",//optional
-		"content" : "<h1><a class='title' href='./'>my lib</a></h1>" 
-		/*
-		"content" : function( args ){ //function for getting content data
-				var res = [.....];
-				if( typeof args["callback"] === "function"){
-					args["callback"]( res );
-				}
-		}//end callback
-		*/
-	};
-	_buildBlock( opt );
+			var opt = {
+				"locationID" : "sitename-block",
+				"templateID" : "tpl-block--sitename",//optional
+				"content" : "<h1><a class='title' href='./'>my lib</a></h1>" 
+			};
+			_buildBlock( opt );
 //---------------------
 			
 			function render_node(){
@@ -2857,6 +2856,144 @@ console.log( _vars["logMsg"] );
 			}
 
 		};//end _insertBlock()
+		
+/*
+		function _wrapContent( opt ){
+			var p = {
+				"data": null,
+				//"type" : "",
+				//"wrapType" : "menu",
+				"templateID" : false,
+				"templateListID" : false
+			};
+			//extend options object
+			for(var key in opt ){
+				p[key] = opt[key];
+			}
+	console.log(p);
+
+			if( !p["data"] ){
+	_log("<p>wrapContent(), error, var data: <b class='text-danger'>" + p["data"] + "</b></p>");
+				return false;
+			}
+			if( p["data"].length === 0 ){
+				return false;
+			}
+			if( !p["templateID"] ){
+	_log("<p>wrapContent(), error, var templateID <b class='text-danger'>is empty</b></p>");
+				return false;
+			}
+			
+			if( !_vars["templates"][p.templateID] ){
+	_log("<p>draw.wrapContent(),  error, not find template, id: <b class='text-danger'>" + p.templateID + "</b></p>");
+				return false;
+			}
+			var html = "";
+
+	//console.log( p["data"].length );
+			p["wrapType"] = "item";
+			if( p["data"].length > 0 ){
+				p["wrapType"] = "list";
+			}
+			
+			switch( p["wrapType"] ){
+				case "item" :
+					html = __formNodeHtml( p["data"], _vars["templates"][ p.templateID ] );
+				break;
+				case "list" :
+					if( !p["templateListID"] ){
+	var msg = "<p>wrapContent(), error, var templateListID <b class='text-danger'>is empty</b></p>";
+	console.log(msg);							
+	_log(msg);
+						return false;
+					}
+					html = __formListHtml( _vars["templates"][ p.templateID ] );
+				break;
+			}//end switch
+			
+	//console.log(html);
+			return html;
+
+			function __formListHtml( _html ){
+				
+				var listHtml = "";
+				for( var n = 0; n < p["data"].length; n++){
+	//console.log( n );
+	//console.log( p["data"][n], typeof p["data"][n], p["data"].length);
+					
+					//form list items
+					var item = p["data"][n];
+						
+					//var itemTpl = _vars["templates"][ p.templateListID];
+					//var itemHtml = __formNodeHtml( item, itemTpl );
+					
+					var itemHtml = _vars["templates"][ p.templateListID];
+					for( var key2 in item){
+	//console.log(key2, item[key2]);
+
+						if( key2 === "childTerms" && item["childTerms"].length > 0){
+							var subOrdList = _vars["templates"][ p.templateID];
+							var itemTpl = _vars["templates"][ p.templateListID];
+							var subOrdListHtml = "";
+							for( var n2 = 0; n2 < item["childTerms"].length; n2++){
+								subOrdListHtml += __formNodeHtml( item["childTerms"][n2], itemTpl );
+							}//next
+	//console.log( subOrdListHtml );
+							subOrdList = subOrdList
+							.replace("list-unstyled", "")
+							.replace("{{list}}", subOrdListHtml);
+	//console.log( subOrdList );
+	//itemHtml += subOrdList;
+							item["childTerms"] = subOrdList;
+							itemHtml = itemHtml.replace("</li>", "{{childTerms}}</li>");
+						} //else {
+							//itemHtml = itemHtml.replace("{{childTerms}}", "");
+						//}
+						
+						if( itemHtml.indexOf("{{"+key2+"}}") !== -1 ){
+	// //console.log(key2, item[key2]);
+							itemHtml = itemHtml.replace("{{"+key2+"}}", item[key2]);
+						}
+					}//next
+						
+					listHtml += itemHtml;
+	//console.log(items);
+	//console.log(listHtml);
+				}//next
+				
+				_html = _html.replace("{{list}}", listHtml);
+				return _html;
+			}//end __formListHtml
+
+			function __formNodeHtml( data, _html ){
+				
+				for( var key in data ){
+	//console.log(key, data[key]);
+
+					if( key === "nodeTerms" && data["nodeTerms"].length > 0){
+						var nodeTermsList = _vars["templates"]["tpl_node_terms"];
+						var itemTpl = _vars["templates"]["tpl-taxonomy-menu_list"];
+						var _listHtml = "";
+						for( var n2 = 0; n2 < data["nodeTerms"].length; n2++){
+							_listHtml += __formNodeHtml( data["nodeTerms"][n2], itemTpl );
+						}//next
+	//console.log( _listHtml );
+						nodeTermsList = nodeTermsList.replace("{{list}}", _listHtml);
+	//console.log( nodeTermsList );
+						data["nodeTerms"] = nodeTermsList;
+					}
+
+					if( _html.indexOf("{{"+key+"}}") !== -1 ){
+	//console.log(key, p["data"][key]);
+						_html = _html.replace( new RegExp("{{"+key+"}}", "g"), data[key] );
+					}
+				}//next
+				
+				return _html;
+			}//end __formNodeHtml()
+			
+		}//end _wrapContent
+*/		
 //====================================
 
 
