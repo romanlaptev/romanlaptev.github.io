a01e3b24c4451e3d49fe18925e124f2f55c729b6
romanlaptev <roman@vbox5>
2018-12-07 12:30:39 +0800

    update webapp_lib

--- webapps/webapp_lib/js/db_admin.js
+++ webapps/webapp_lib/js/db_admin.js
@@ -1,50 +1,74 @@
 ﻿//console.log("module sharedFunc:", typeof sharedFunc, sharedFunc);
-	var func = sharedFunc();
-console.log("func:", func);
-		
+var func = sharedFunc();
+//console.log("func:", func);
+
+var _vars = {
+	"logMsg": "",
+	"config": {
+		"useCache": true,
+		"dbName": "db1"
+	},
+	"tests": {
+		"indexedDB": false,
+		"WebSQL": false,
+		"localStorage": false,
+		"template": "<li>\
+<b>{{name}}</b> : <span class='text-info text-uppercase'><b>{{result}}</b></span>\
+<p><small>{{msg}}</small></p>\
+</li>	"
+	}
+};
+console.log("_vars:", _vars);
+
+var res = initCache();
+window.onload = function(e){
+console.log("onLoad", e);
+//---------------------------------
+		var ua = document.getElementById("ua");
+		var tpl = ua.innerHTML;
+		var html = tpl
+		.replace("{{userAgent}}", navigator.userAgent);
+		ua.innerHTML = html;
+
+		var tests_out = document.getElementById("tests-out");
+		tests_out.innerHTML = _vars["tests"]["html"];
+//--------------------------------------
+
+	if(res){
+			runApp();
+	} else {
+	_vars["logMsg"] = "error, not support web-storages...";
+	 func.log("<div class='alert alert-danger'>" + _vars["logMsg"] + "</div>");
+	//console.log( _vars["logMsg"] );
+	}
+};//end window.load	
+
+function runApp(){
+console.log("function runApp()");
+}//end runApp()
+
 $(document).ready(function(){
 console.log("Ready!", arguments);
 
-	$("#btn-clear-log").on("click", function(e){
-		e.preventDefault();
-		$("#log").html("");
-	});//end event
+	// $("#btn-clear-log").on("click", function(e){
+		// e.preventDefault();
+		// $("#log").html("");
+	// });//end event
 	
-	$(".btn-test").on("click", function(e){
-		e.preventDefault();	
-		var GET = parseGetParams(e.target.href);
-		var nameTestFunc = GET["func"];
-//console.log(nameTestFunc, typeof nameTestFunc);
-//console.log( window[nameTestFunc], typeof window[nameTestFunc]);
-		window[nameTestFunc]();
-	});//end event
+	// $(".btn-test").on("click", function(e){
+		// e.preventDefault();	
+		// var GET = parseGetParams(e.target.href);
+		// var nameTestFunc = GET["func"];
+// //console.log(nameTestFunc, typeof nameTestFunc);
+// //console.log( window[nameTestFunc], typeof window[nameTestFunc]);
+		// window[nameTestFunc]();
+	// });//end event
 	
 });//end ready
 
 
-	function test_storage() {
-	  try {
-		return 'localStorage' in window && window['localStorage'] !== null;
-	} catch (e) {
-console.dir(e);	
-		return false;
-	  }
-	}
-
-	// Force localStorage to be the backend driver.
-	//localforage.setDriver(localforage.LOCALSTORAGE);
-
-	// Supply a list of drivers, in order of preference.
-	//localforage.setDriver([localforage.WEBSQL, localforage.INDEXEDDB]);
-
-	localforage.ready(function() {
-		console.log('localForage ready', arguments);
-		console.log('localforage.driver():', localforage.driver());
-
-	});
-	
+/*	
 window.onload = function(){
-
 
 	if( document.querySelector)
 	{
@@ -212,3 +236,129 @@ console.log('Iteration has completed');
 	
 
 };//end window.load	
+*/
+
+function initCache(){
+	
+	runTests();
+
+	if ( !_vars["tests"]["localStorage"] &&
+			!_vars["tests"]["WebSQL"] &&
+				!_vars["tests"]["indexedDB"]){
+		_vars["config"]["useCache"] = false;
+		return false;
+	}
+
+//-----------------	
+	var _driver = [];
+	if( _vars["tests"]["indexedDB"] ){
+		_driver.push(localforage.INDEXEDDB);
+	}
+	
+	if( _vars["tests"]["WebSQL"] ){
+		_driver.push(localforage.WEBSQL);
+	}
+	
+	if( _vars["tests"]["localStorage"] ){
+		_driver.push(localforage.LOCALSTORAGE);
+	}
+	
+			localforage.config({
+				// driver: [localforage.INDEXEDDB,
+						 // localforage.WEBSQL,
+						 // localforage.LOCALSTORAGE],
+
+				//driver: [localforage.WEBSQL],
+				//driver: [localforage.LOCALSTORAGE],
+				driver: _driver,
+				name: _vars["config"]["dbName"]
+			});
+
+			localforage.ready(function() {
+console.log('localForage ready');
+console.log('localforage.driver():', localforage.driver());
+
+console.log( "localforage config: ", localforage._config );
+console.log( "localforage version: " + localforage._config.version );
+			});
+
+			localforage.length(function(err, numberOfKeys) {
+console.log('localforage number of keys - ' + numberOfKeys);
+console.dir(err);
+			});
+			return true;
+}//end initCache()
+
+function runTests(){
+
+	var tests = [];
+
+	var test = {
+		"name" : "indexedDB",
+		"result" : false
+	};
+	if (!window.indexedDB) {
+		test["msg"] = "IndexedDB not supported, ";
+		// test["msg"] += "<br>window.indexedDB = " + window.indexedDB;
+		// test["msg"] += "<br>window.mozIndexedDB = " + window.mozIndexedDB;
+		// test["msg"] += "<br>window.webkitIndexedDB = " + window.webkitIndexedDB;
+		// test["msg"] += "<br>window.msIndexedDB = " + window.msIndexedDB;
+	} else {
+		test["result"] = true;
+		// test["msg"] = "<br>window.indexedDB = " + window.indexedDB;
+		// test["msg"] += "<br>window.mozIndexedDB = " + window.mozIndexedDB;
+		// test["msg"] += "<br>window.webkitIndexedDB = " + window.webkitIndexedDB;
+		// test["msg"] += "<br>window.msIndexedDB = " + window.msIndexedDB;
+		_vars["tests"]["indexedDB"] = true;
+	}
+	tests.push(test);
+//--------------------------------------
+
+	var test = {
+		"name" : "Web SQL database",
+		"result" : false
+	};
+	if (window.openDatabase) {
+		test["result"] = true;
+		_vars["tests"]["WebSQL"] = true;
+	}
+	tests.push(test);
+//--------------------------------------
+
+	var test = {
+		"name" : "Local storage",
+		"result" : false
+	};
+	
+	  // try {
+		// return 'localStorage' in window && window['localStorage'] !== null;
+	// } catch (e) {
+// console.dir(e);	
+	  // }
+	if( window['localStorage'] ){
+		test["result"] = true;
+		_vars["tests"]["localStorage"] = true;
+	}
+	tests.push(test);
+//--------------------------------------
+
+//----------------- form html
+	var test_tpl = _vars["tests"]["template"];
+//console.log( test_tpl);	
+	var html = "";
+	for (var n = 0; n < tests.length; n++){
+		if( tests[n] ){
+			var msg = tests[n]["msg"];
+			if(!msg){
+				var msg = "";
+			}
+			
+			html += test_tpl
+			.replace("{{name}}", tests[n]["name"])
+			.replace("{{result}}", tests[n]["result"])
+			.replace("{{msg}}", msg);
+		}
+	}//next
+
+	_vars["tests"]["html"] = html;
+}//end runTests()
