2d022bfdd76623629fad65c205e6ee23b363a842
romanlaptev <roman@vbox5>
2018-12-19 11:29:16 +0800

    update webapp_lib

---
+++ git_command.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#switch to old commit
+#git checkout 22ec5e35832a146596a59a4bd5cf4795274ee7e3
+
+#return
+git checkout dev
+
+#git checkout -b test
+#git chekout master
+
+#Push local branch 'test' on GitHub
+#git push origin origin:refs/heads/test
