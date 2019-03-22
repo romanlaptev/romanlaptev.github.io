d01516394adc3758a5eafa637dfe5094ad767433
romanlaptev <roman@vbox5>
2019-03-22 16:58:41 +0800

    update custom colors

--- projects/custom_colors/js/main.js
+++ projects/custom_colors/js/main.js
@@ -5,33 +5,71 @@ var _vars = {
 	colors: [
 		{
 			"styleName": "bg-darkblue",
-			"code": " #004586"
+			"code": "#004586"
 		},
 		{
 			"styleName": "bg-darkblue2",
-			"code": "  #000080"
+			"code": " #000080"
 		},
 		{
 			"styleName": "bg-darkblue3",
-			"code": " #0e1f40"
+			"code": "#0e1f40"
 		},
 		{
 			"styleName": "bg-darkblue4",
-			"code": " #062237"
+			"code": "#062237"
 		},
 		{
 			"styleName": "bg-darkblue5",
-			"code": " #153449"
+			"code": "#153449"
 		},
 		{
-			"styleName": ".bg-darkblue-tango",
-			"code": " #0c253c"
+			"styleName": "bg-darkblue-tango",
+			"code": "#0c253c"
 		},
 		{
-			"styleName": ".bg-darkblue-tango2",
-			"code": " #001b33"
+			"styleName": "bg-darkblue-tango2",
+			"code": "#001b33"
+		},
+		{
+			"styleName": "bg-lightblue",
+			"code": "#0084d1"
+		},
+		{
+			"styleName": "bg-lightblue2",
+			"code": "#083caf"
+		},
+		{
+			"styleName": "bg-lightblue3",
+			"code": "#6aa6c0"
+		},
+		{
+			"styleName": "bg-lightblue4",
+			"code": "#C9E0ED"
+		},
+		{
+			"styleName": "bg-lightblue5",
+			"code": "#BBD8E9"
+		},
+
+		{
+			"styleName": "bg-lightsteelblue",
+			"code": "#b0c4de"
+		},
+		{
+			"styleName": "bg-lightsteelblue2",
+			"code": "#83A0C5"
+		},
+		{
+			"styleName": "bg-lightsteelblue3",
+			"code": "#3b5a6f"
+		},
+		{
+			"styleName": "bg-lightsteelblue4",
+			"code": "#b6cfed"
 		}
-	]
+
+	]//end colors
 }
 console.log(_vars);
 
@@ -60,7 +98,30 @@ function createPalette(){
 		
 		newDiv = document.createElement("div");
 		newDiv.className = "block";
-		newDiv.setAttribute("style", "background-color:"+_vars["colors"][n]["code"]+";");
+		var _color = _vars["colors"][n]["code"];
+		newDiv.setAttribute("style", "background-color:"+_color+";");
+		
+		//inverse color
+/*		
+var r = "00";
+var g = "84";
+var b = "d1";
+r = (255 - parseInt(r, 16)).toString(16);
+g = (255 - parseInt(g, 16)).toString(16);
+b = (255 - parseInt(b, 16)).toString(16);
+console.log(r,g,b); // осталось только нолики дописать
+*/		
+		var r = _color.substr(1,2);
+		var g = _color.substr(3,2);
+		var b = _color.substr(5,2);
+		r = (255 - parseInt(r, 16)).toString(16);
+		g = (255 - parseInt(g, 16)).toString(16);
+		b = (255 - parseInt(b, 16)).toString(16);
+//console.log(r,g,b);
+		
+		newDiv.style.color="#"+r+g+b;
+		newDiv.setAttribute("title", "background-color: "+_color);
+		
 		newDiv.innerHTML = "<p>" + _vars["colors"][n]["code"] + "</p>";
 		newDiv.innerHTML += "<p>" + _vars["colors"][n]["styleName"] + "</p>";
 		
