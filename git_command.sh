4625ab06d394f6c0e68d04d1d624a1bd7ff82412
romanlaptev <roman@vbox5>
2019-04-02 13:29:24 +0800

    update video app

--- git_command.sh
+++ git_command.sh
@@ -2,9 +2,10 @@
 
 #switch to old commit
 #git checkout 22ec5e35832a146596a59a4bd5cf4795274ee7e3
+git checkout 385d0fd31e252d76e4ec8b1b43029fff76106337
 
 #return
-git checkout dev
+#git checkout dev
 
 #git checkout -b test
 #git chekout master
