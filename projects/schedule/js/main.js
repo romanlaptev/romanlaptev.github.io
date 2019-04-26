71cf71b533b761d106827506145d45ec0825bc31
romanlaptev <roman@vbox5>
2019-04-26 12:32:57 +0800

    update projects, schedule

---
+++ projects/schedule/js/main.js
@@ -0,0 +1,7 @@
+﻿var func = sharedFunc();
+//console.log("func:", func);
+
+window.onload = function(){
+//console.log("onload");
+	func.log( "<p class='alert alert-info'>"+navigator.userAgent +"</p>");
+};//end load
