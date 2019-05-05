28b3506c52d8aabab4541ecc7165698c34b87f21
romanlaptev <roman-laptev@yandex.ru>
2019-05-05 16:46:47 +0700

    update schedule app

--- projects/schedule/js/test.js
+++ projects/schedule/js/test.js
@@ -8,7 +8,7 @@ var logMsg;
 //console.log(arguments);
 //});
 //my_func({a:1});
-
+/*
   var url = "https://api.rasp.yandex.net/v3.0/search/?from=851508&to=851635&apikey=b07a64bc-f237-4e79-9efb-b951ec68eaf7&date=2019-04-26&transport_types=suburban&system=esr&show_systems=esr";
 	//var url = "http://i5/webapps/";
 	//var url="https://github.com/romanlaptev/romanlaptev.github.io/tree/master/test_code";
@@ -30,7 +30,38 @@ var logMsg;
 //for( var key in iframe.contentDocument){
 //console.log(key, iframe.contentDocument[key]);
 //}
+*/
 
+var loadFrame = function(){
+	var iframe_src = document.querySelector("#iframe-src");
+	var url = iframe_src.value;
+	if( url.length === 0){
+		logMsg = "enter url...";
+		log.innerHTML += "<p class='alert alert-danger'>" +logMsg+"</p>";
+		iframe_src.className += " invalid";
+		return;
+	}
+
+	
+//console.log( test_frame );
+	try	{
+		var iframe = window.frames[ test_frame.name ];
+//for(var item in iframe)
+//{
+//console.log( item +" = "+ iframe[item]);
+//}
+//alert (document.all.test_frame.src);
+//alert (iframe.location);
+		iframe.location = iframe_src.value;
+//alert (frames[0].document.body.innerHTML);
+//alert( iframe.document.body.innerHTML );
+	}
+	catch(e){
+		logMsg = "<p class='alert alert-danger'><b>error</b>: " + e.message + "</p> ";
+		log.innerHTML += "<p>" +logMsg+"</p>";
+console.log(e);
+	}
+}//end loadFrame()
 
 function getFrameInfo( frame_tag ){
 console.log( frame_tag );
@@ -44,12 +75,19 @@ console.log( frame_tag );
 	try{
 		logMsg = "<b>iframe location</b> = " + iframe.location;
 		log.innerHTML += "<p class='alert alert-info'> " + logMsg + "</p>";
+console.log( iframe.location );
+		if( iframe.location.href === "about:blank"){
+			return;
+		}
 	}
 	catch(e){
 		logMsg = "<b>iframe location, access error</b>: " + e.message;
 		log.innerHTML += "<p class='alert alert-danger'> " + logMsg + "</p>";
+		log.innerHTML += "<p class='alert alert-danger'> " + e + "</p>";
 console.log(e);
+		return;
 	}
+
 
 	try{
 		logMsg = "<b>iframe.document</b> = " + iframe.document;
