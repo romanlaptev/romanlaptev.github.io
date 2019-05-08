c0e498d824da57bd49390216f9d29cac2c8d67c9
romanlaptev <roman@vbox5>
2019-05-08 17:19:55 +0800

    update projects, schedule

--- projects/schedule/js/schedule_app.js
+++ projects/schedule/js/schedule_app.js
@@ -33,7 +33,6 @@ var webApp = {
 				"esr_code" : 851635
 			},
 		},
-		
 		"apikey" : "b07a64bc-f237-4e79-9efb-b951ec68eaf7",
 		"logMsg" : "",
 		
@@ -45,8 +44,8 @@ https://api.rasp.yandex.net/v3.0/copyright/?apikey=b07a64bc-f237-4e79-9efb-b951e
 		
 		"DB" : {
 			//"dataUrl" : "data/2019-04-26.xml",
-			"dataUrl" : "data/2019-04-26.json",
-/*
+			//"dataUrl" : "data/2019-04-26.json",
+
 			"dataUrl" : "https://cors-anywhere.herokuapp.com/\
 https://api.rasp.yandex.net/v3.0/search/?\
 from={{from_code}}&\
@@ -56,7 +55,7 @@ date={{date}}&\
 transport_types=suburban&\
 system=esr&\
 show_systems=esr",
-*/
+
 			"dbType" : "" //application/xml 
 		},
 
