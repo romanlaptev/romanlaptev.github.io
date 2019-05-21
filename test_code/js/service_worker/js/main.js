11263cd01713c359201681a4225960d881e34c57
romanlaptev <roman@vbox5>
2019-05-21 12:11:52 +0800

    update test code

--- test_code/js/service_worker/js/main.js
+++ test_code/js/service_worker/js/main.js
@@ -1,37 +1,32 @@
-	var func = sharedFunc();
+var func = sharedFunc();
 //console.log("func:", func);
 
-	var logMsg;
-	logMsg = navigator.userAgent;
-	func.logAlert(logMsg, "info");
-	
-	var test =  typeof window.Promise !== "undefined";
-	logMsg = "Promise support:" + test;
-	if( test ){
-		func.logAlert(logMsg, "success");
-	} else {
-		func.logAlert(logMsg, "error");
-	}
-	
-	//test = 'serviceWorker' in navigator;
-	//logMsg = "serviceWorker support: " + test;
-	//if( test ){
-		//func.logAlert(logMsg, "success");
-		registerServiceWorker();
-	//} else {
-		//func.logAlert(logMsg, "error");
-	//}
+var support=false;
 
-	if( window.location.protocol !== "https:"){
-		logMsg = "error,  serviceWorker requires 'https:' protocol....";
-		logMsg += "but used: " + window.location.protocol;
-		func.logAlert(logMsg, "error");
-	}
-	
-	
+var logMsg;
+logMsg = navigator.userAgent;
+func.logAlert(logMsg, "info");
+
+var test =  typeof window.Promise !== "undefined";
+logMsg = "Promise support:" + test;
+if( test ){
+	func.logAlert(logMsg, "success");
+} else {
+	func.logAlert(logMsg, "error");
+}
+
+if( window.location.protocol !== "https:"){
+	logMsg = "error,  serviceWorker requires 'https:' protocol....";
+	logMsg += "but used: " + window.location.protocol;
+	func.logAlert(logMsg, "error");
+}
+
+registerServiceWorker();
+defineEvents();
+
 function registerServiceWorker() {
 	var test = "serviceWorker" in navigator;
-console.log(test);	
+//console.log(test);	
 	if (!test) {
 		logMsg = "CLIENT: service worker is not supported."
 		func.logAlert(logMsg, "error");
@@ -58,31 +53,8 @@ func.logAlert( logMsg, "info" );
 logMsg="Service worker active";
 func.logAlert( logMsg, "info" );
 			}
-//-----------------------------
-/*
-caches.open( "static-cache-v1" ).then( function(cache) {
-console.log("TEST:", cache);	
-
-	cache.keys().then( function(keyList) {
-console.log("CACHE key list:", keyList);
-	});
-	
-});
-*/
-
-/*
-caches.keys().then( function(keyList) {
-console.log("CACHE key list:", keyList);
-
-			return Promise.all( keyList.map( function (key) {
-console.log(key);
-						//return caches.delete(key);
-				})
-			);
-	
-});
-*/				
-//----------------------------
+			
+			support = true;
 		}, 
 
 		function(err) {
@@ -100,3 +72,64 @@ console.log(error);
 	});//end event
 
 }//end registerServiceWorker()
+
+function defineEvents(){
+
+	var btn_clear_log = document.querySelector("#btn-clear-log");
+	btn_clear_log.onclick = function(){
+		log.innerHTML = "";
+	};
+	
+	var cacheNameField = document.querySelector("#cache-name");
+	
+	var btn_list_cache_keys = document.querySelector("#btn-list-cache-keys");
+	btn_list_cache_keys.onclick = function(e){
+//console.log(e);
+		if(!support){
+			return false;
+		}
+		
+		var cacheName = cacheNameField.value;
+//console.log(cacheName);
+		if( !cacheName || cacheName.length===0 ){
+			return false;
+		}
+
+		caches.open( cacheName ).then( function(cache) {
+console.log("TEST:", cache);	
+			cache.keys().then( function(keyList) {
+console.log("CACHE key list:", keyList);
+				for( var n = 0; n < keyList.length; n++){
+func.logAlert( keyList[n]["url"], "info");					
+				}//next
+				
+			});
+	
+		});
+		
+	}//end event
+
+	var btn_list_caches = document.querySelector("#btn-list-caches");
+	btn_list_caches.onclick = function(e){
+//console.log(e);
+		if(!support){
+			return false;
+		}
+
+		caches.keys().then( function(keyList) {
+console.log("CACHE key list:", keyList);
+func.log("");					
+			for( var n = 0; n < keyList.length; n++){
+func.logAlert( keyList[n], "info");					
+			}//next
+				
+					// return Promise.all( keyList.map( function (key) {
+		// console.log(key);
+								// //return caches.delete(key);
+						// })
+					// );
+		});
+
+	}//end event
+	
+}//end defineEvents()
