0817104ffabb896ccc7ca67df3a84af109695983
romanlaptev <roman@vbox5>
2019-07-01 12:46:26 +0800

    update

--- git_command.sh
+++ git_command.sh
@@ -12,3 +12,6 @@ git checkout 385d0fd31e252d76e4ec8b1b43029fff76106337
 
 #Push local branch 'test' on GitHub
 #git push origin origin:refs/heads/test
+
+#create submodule
+#git submodule add https://github.com/romanlaptev/bookmarks.git projects/bookmarks/bookmarks-cache
