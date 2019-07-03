d202f27d57f7a1fa98451710da18a96a72786c41
romanlaptev <roman@vbox5>
2019-07-03 10:11:24 +0800

    update

--- git_command.sh
+++ git_command.sh
@@ -19,3 +19,5 @@ git checkout 385d0fd31e252d76e4ec8b1b43029fff76106337
 #pull submodule on remote repositories
 #sudo git submodule update --init --recursive
 #git submodule update
+
+#git config --global push.default current
