fad78b44d71e9cded063ca1d332f8e82ae5fb2e7
romanlaptev <roman@vbox5>
2019-10-08 14:44:48 +0800

    update geolocation app

---
+++ projects/geolocation/js/main.js
@@ -0,0 +1,20 @@
+//console.log("module sharedFunc:", typeof sharedFunc, sharedFunc);
+//var func = sharedFunc();
+//console.log("func:", func);
+
+window.onload = function(){
+console.log("window event onload");
+//func.log( navigator.userAgent );
+
+	//Start webApp
+	//if( typeof webApp === "object"){
+		//_runApp();
+	//}
+
+	//function _runApp(){
+		//webApp.init(function(){
+//console.log("end webApp initialize....");
+		//});//end webApp initialize
+	//}//end _runApp()
+
+};//end window.load
