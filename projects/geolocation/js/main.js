e8b2fd3d0d30af3858e9680787e7a2694069ee86
romanlaptev <roman@vbox5>
2019-10-09 11:30:16 +0800

    update geolocation app

--- projects/geolocation/js/main.js
+++ projects/geolocation/js/main.js
@@ -2,24 +2,45 @@
 var func = sharedFunc();
 console.log("func:", func);
 
-window.onload = function(){
-console.log("window event onload");
-var logMsg;
-logMsg = navigator.userAgent;
-func.logAlert(logMsg, "info");
+	var support = false;
+	var logMsg;
 
-defineEvents();
+window.onload = function(){
+
+	logMsg = navigator.userAgent;
+	func.logAlert(logMsg, "info");
+
+	defineEvents();
+
+	//--------------------------
+	var test =  typeof navigator.geolocation !== "undefined";
+	logMsg = "navigator.geolocation support: " + test;
+	if ( test ) {
+		func.logAlert(logMsg, "success");
+		support = true;
+	} else {
+		func.logAlert(logMsg, "danger");
+	}
+	
+	//--------------------------
+	if( window.location.protocol !== "https:"){
+		support = false;
+		logMsg = "error, <b>navigator.geolocation</b> requires <b>'https:'</b> protocol....";
+		func.logAlert( logMsg, "error" );
+	} else {
+		support = true;
+	}
 
 	//Start webApp
-	//if( typeof webApp === "object"){
-		//_runApp();
-	//}
+	if( support ){
+		_runApp();
+	}
 
-	//function _runApp(){
-		//webApp.init(function(){
-//console.log("end webApp initialize....");
-		//});//end webApp initialize
-	//}//end _runApp()
+	function _runApp(){
+		var _app = App();
+console.log("_app:", _app);
+		_app.init();
+	}//end _runApp()
 
 };//end window.load
 
@@ -28,8 +49,38 @@ defineEvents();
 function defineEvents(){
 
 	var btn_clear_log = func.getById("btn-clear-log");
-	btn_clear_log.onclick = function(){
+	btn_clear_log.onclick = function( event ){
+//console.log("click...", e);			
+		event = event || window.event;
+		var target = event.target || event.srcElement;
+		if (event.preventDefault) { 
+			event.preventDefault();
+		} else {
+			event.returnValue = false;				
+		}
 		log.innerHTML = "";
-	};
+	};//end event
+
+/*
+//------------------------------------------------------------------
+	$("#btn-toggle-log").on("click", function(event){
+//console.log("click...", e);			
+		event = event || window.event;
+		//var target = event.target || event.srcElement;
+		if (event.preventDefault) { 
+			event.preventDefault();
+		} else {
+			event.returnValue = false;				
+		}
+		
+		if( webApp.vars["log"].style.display==="none"){
+			webApp.vars["log"].style.display="block";
+			webApp.vars["btnToggle"].innerHTML="-";
+		} else {
+			webApp.vars["log"].style.display="none";
+			webApp.vars["btnToggle"].innerHTML="+";
+		}
+	});//end event
+*/
 
 }//end defineEvents()
