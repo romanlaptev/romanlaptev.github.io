1151b6913ec429a2d2d960859859ef54d7300320
romanlaptev <roman@vbox5>
2020-02-14 16:46:26 +0800

    update music app

--- webapps/webapp_music/nodejs/index.js
+++ webapps/webapp_music/nodejs/index.js
@@ -1,4 +1,6 @@
 var _http = require("http");
+var _path = require("path");
+var _fs = require("fs");
 
 _http.createServer( _requestListener ).listen(3000, function(){
 console.log("Server has been started.");
@@ -11,7 +13,27 @@ function _requestListener( req, resp){
 //console.log("-- response: ", resp);
 
 console.log("Request received.");
-	resp.writeHead(200, {"Content-Type": "text/plain"});
-	resp.write("Hello World!!!");
-	resp.end();
+	//resp.writeHead(200, {"Content-Type": "text/plain"});
+	//resp.writeHead(200, {"Content-Type": "text/html"});
+	//resp.write("<h1>Hello World</h1>");
+	
+console.log( req.url );
+	if( req.url === "/"){
+		
+		//var _filePath = _path.join( __dirname, "nb.txt");
+		var _filePath = "../app.html";
+		_fs.readFile( _filePath, function(err, data ){
+//console.log("readFile, err:", err);
+			if(err){
+				throw(err);
+			}
+			resp.writeHead( 200, {"Content-Type": "text/html"});
+			resp.end( data );
+			
+		});
+	}
+	
+	
+	//resp.end("Hello world! !");
+	//resp.end();
 }//end
---
+++ webapps/webapp_music/nodejs/run.cmd
@@ -0,0 +1,2 @@
+npm run start
+rem npm run dev
