523c54d54444846161b5ee5944178f9352712a36
romanlaptev <roman-laptev@yandex.ru>
2020-03-12 11:07:07 +0700

    update php test code

--- test_code/php/test_oop.php
+++ test_code/php/test_oop.php
@@ -35,12 +35,30 @@ $object1->getName();
 
 //======================================================
 class parentClass {
+
+	private $prop1 = "This is public text!!!";	
+	private $prop2 = "This is secret PRIVATE text!!!";	
+	public static $prop3 = 10;
+
+	public function func_public(){//GETTER for private property
+		print( $this->prop1."\n");
+	}//end
+
+	//private function func_test(){// Access error!!!!!
+	protected function func_test(){
+		print( $this->prop2."\n");
+	}//end
+
+	public static function func_static(){
+		echo  "-- public static function func_static()\n";
+	}//end
+
 	function __construct(){
 echo "Object of class ".get_class( $this)." was created. "."\n";
 	}//end constructor
 
 	function method1(){
-echo "Running method of class ".get_class( $this)."\n";
+echo "Running method of class 'parentClass'\n";
 	}//end
 
 	function __destruct(){
@@ -49,9 +67,20 @@ echo "Object of class ".get_class( $this)." was destroyed. "."\n";
 
 }//end class
 
-class childClass extends parentClass{
+class childClass extends parentClass {
+
+	const GOOD = 1;
+	const BAD = 0;
+
 	function __construct(){
 		parent::__construct();
+
+		//try{
+			parent::func_public();
+		//} catch ( Exception $e ){
+//echo "Exception: ",  $e->getMessage(), "\n";
+		//}
+
 	}//end constructor
 
 	function __destruct(){
@@ -61,7 +90,21 @@ class childClass extends parentClass{
 
 }//end class
 
+class thirdClass extends parentClass {
+	public function method3(){
+		$this->func_test();
+	}//end
+}//end class
 
 $obj = new childClass();
+
+//------------------
+$obj3 = new thirdClass();
+$obj3->method3();
+
+//------------------
+parentClass::func_static();
+parentClass::$prop3++;//access to the static property, only :: syntax!!!!
+echo "Static property: ".parentClass::$prop3."\n";
 
 ?>
