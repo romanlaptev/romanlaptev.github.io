3a8ec48f40435eeef3ec1c0bbfd1c42760cb5935
romanlaptev <roman-laptev@yandex.ru>
2020-03-12 10:18:01 +0700

    update php test code

--- test_code/php/test_oop.php
+++ test_code/php/test_oop.php
@@ -1,15 +1,67 @@
 <?php
-class FirstClass {
-	public $name = "Roman";
-	public $age = 44;
 
-	public function method1(){
-echo "Name: ".$this->name."\n";
-echo "Age: ".$this->age."\n";
+class FirstClass {
+	private $name;
+	private $age;
+	private $className;
+	private $greeting;
+
+	//public function FirstClass(){//PHP4
+	function __construct($argv){//PHP5
+
+		$this->className = get_class( $this);
+echo "Object of class ".$this->className." was created. "."\n";
+
+		$this->greeting = $argv;
+		$this->name = "Roman";
+		$this->age = 44;
+	}//end constructor
+
+	public function getName(){
+echo $this->greeting;
+echo $this->name.", ";
+echo "my age: ".$this->age."\n";
+	}//end
+
+	function __destruct(){
+echo "Object of class ".$this->className." was destroyed. "."\n";
 	}//end
 
 }//end class
 
-$class1 = new FirstClass;
-$class1->method1();
+
+$object1 = new FirstClass("Hello, I am ");
+$object1->getName();
+
+//======================================================
+class parentClass {
+	function __construct(){
+echo "Object of class ".get_class( $this)." was created. "."\n";
+	}//end constructor
+
+	function method1(){
+echo "Running method of class ".get_class( $this)."\n";
+	}//end
+
+	function __destruct(){
+echo "Object of class ".get_class( $this)." was destroyed. "."\n";
+	}//end
+
+}//end class
+
+class childClass extends parentClass{
+	function __construct(){
+		parent::__construct();
+	}//end constructor
+
+	function __destruct(){
+		parent::method1();
+		parent::__destruct();
+	}//end
+
+}//end class
+
+
+$obj = new childClass();
+
 ?>
