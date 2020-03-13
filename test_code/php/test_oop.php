a0c3572c30611c670d91d8e92d166d268cf9060c
romanlaptev <roman-laptev@yandex.ru>
2020-03-13 12:58:42 +0700

    update php code

--- test_code/php/test_oop.php
+++ test_code/php/test_oop.php
@@ -1,48 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>test PHP</title>
-	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	<meta http-equiv="Pragma" content="no-cache">
-	<meta http-equiv="Expires" content="0">	
-	<meta http-equiv="X-UA-Compatible" content="IE=10">
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">	
-<style>
-.alert {
-	margin-bottom: 3px !important;
-	padding: 10px !important;
-}
-.panel-body{
-	padding: 5px;
-}
-</style>
-</head>
-
-<body>
-	<div class="container">
-		<div class="page-header">
-			<h3>test PHP: OOP</h3>
-		</div>
-<pre>
-// run local web-server
-php -S localhost:8000 -t test_code/php
-
-Мэтт Зандстра, PHP. Объекты, шаблоны и методики программирования (2-е издание)
-php._obekty_shablony_i_metodiki_programmirovaniya.djvu
-p82
-
-https://www.php.net/manual/ru/language.oop5.overloading.php
-Перегрузка 
-
-</pre>
-	<div class="panel">
-		<div class="panel-body">
-
 <?php
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 1);
+
+$sapi_type = php_sapi_name();
+
+$_vars["runType"] = "";
+if ( $sapi_type == 'apache2handler' || $sapi_type == 'cli-server') {
+	$_vars["runType"] = "web";
+	formPageHeader();
+}
+if ( $sapi_type == 'cli') {
+	$_vars["runType"] = "console";
+}
+$msg = "Run method (php_sapi_name): ". $sapi_type;
+echo logAlert($msg, "info");
 
 class FirstClass {
 	public$name;
@@ -54,7 +25,7 @@ class FirstClass {
 	function __construct($argv){//PHP5
 
 		$this->className = get_class( $this);
-		$msg = "Object of class ".$this->className." was created. "."\n";
+		$msg = "Object of class ".$this->className." was created.";
 		echo logAlert($msg, "success");
 
 		$this->greeting = $argv;
@@ -65,24 +36,24 @@ class FirstClass {
 	public function getName(){
 		$msg = $this->greeting;
 		$msg .= $this->name.", ";
-		$msg .= "my age: ".$this->age."\n";
+		$msg .= "my age: ".$this->age;
 		echo logAlert($msg, "info");
 	}//end
 
 	function __get( $property){
-		$msg = "__get(), error, property by name <b>".$property."</b> not defined "."\n";
+		$msg = "__get(), error, property by name <b>".$property."</b> not defined ";
 		echo logAlert($msg, "error");
 	}//end
 
 	function __set( $property, $value){
 		$msg = "__set(), error, ";
-		$msg .= "do not set <b>".$property."</b> with value ".$value."\n";
+		$msg .= "do not set <b>".$property."</b> with value ".$value;
 		echo logAlert($msg, "error");
 	}//end
 
 	function __call( $method, $arg_arr){
 		$msg = "__call(), error, ";
-		$msg .= "method by name <b>".$method."</b> not defined "."\n";
+		$msg .= "method by name <b>".$method."</b> not defined ";
 		echo logAlert($msg, "error");
 echo "arguments: <pre>";
 print_r( $arg_arr );
@@ -91,19 +62,23 @@ echo "</pre>";
 
 	function __isset( $property ){
 		$msg = "__isset(), error, ";
-		$msg .= "property by name <b>".$property."</b> not defined "."\n";
+		$msg .= "property by name <b>".$property."</b> not defined ";
 		echo logAlert($msg, "error");
 	}//end
 
 	function __unset( $property ){
 		$msg = "__unset(), error, ";
-		$msg .= "do not destroy <b>".$property."</b> "."\n";
+		$msg .= "do not destroy <b>".$property."</b> ";
 		echo logAlert($msg, "error");
 	}//end
 
 	function __destruct(){
-		$msg = "Object of class ".$this->className." was destroyed. "."\n";
+global $_vars;
+		$msg = "Object of class ".$this->className." was destroyed. ";
 		echo logAlert($msg, "info");
+if ( $_vars["runType"] == "web") {
+		formPageFooter();
+}
 	}//end
 
 }//end class
@@ -134,26 +109,26 @@ class parentClass {
 
 	//private function func_test(){// Access error!!!!!
 	protected function func_test(){
-		print( $this->prop2."\n");
+		print( $this->prop2 );
 	}//end
 
 	public static function func_static(){
-		$msg = "public static function func_static()"."\n";
+		$msg = "public static function func_static()";
 		echo logAlert($msg, "info");
 	}//end
 
 	function __construct(){
-		$msg = "Object of class ".get_class( $this)." was created. "."\n";
+		$msg = "Object of class ".get_class( $this)." was created. ";
 		echo logAlert($msg, "success");
 	}//end constructor
 
 	function method1(){
-		$msg = "Running method of class 'parentClass'"."\n";
+		$msg = "Running method of class 'parentClass'";
 		echo logAlert($msg, "info");
 	}//end
 
 	function __destruct(){
-		$msg = "Object of class ".get_class( $this)." was destroyed. "."\n";
+		$msg = "Object of class ".get_class( $this)." was destroyed. ";
 		echo logAlert($msg, "info");
 	}//end
 
@@ -197,7 +172,7 @@ $obj3->method3();
 //------------------
 parentClass::func_static();
 parentClass::$prop3++;//access to the static property, only :: syntax!!!!
-$msg = "Static property: ".parentClass::$prop3."\n";
+$msg = "Static property: ".parentClass::$prop3;
 echo logAlert($msg, "info");
 
 //============================ Abstract class
@@ -241,38 +216,115 @@ class testChildClass extends finalClass {//error, do not inherit from final clas
 */
 
 function logAlert( $msg, $level){
-
+//global $sapi_type;
+global $_vars;
 	switch ($level) {
 		case "info":
-			return "<div class='alert alert-info'>".$msg."</div>";
+			if ( $_vars["runType"] == "web" ) {
+				return "<div class='alert alert-info'>".$msg."</div>";
+			}
+			if ( $_vars["runType"] == "console" ) {
+				return $msg."\n";
+			}
 		break;
 		
 		case "warning":
-			return "<div class='alert alert-warning'>".$msg. "</div>";
+			if ( $_vars["runType"] == "web" ) {
+				return "<div class='alert alert-warning'>".$msg. "</div>";
+			}
+			if ( $_vars["runType"] == "console" ) {
+				return $msg."\n";
+			}
 		break;
 		
 		case "danger":
 		case "error":
-			return "<div class='alert alert-danger'>".$msg. "</div>";
+			if ( $_vars["runType"] == "web" ) {
+				return "<div class='alert alert-danger'>".$msg. "</div>";
+			}
+			if ( $_vars["runType"] == "console" ) {
+				return $msg."\n";
+			}
 		break;
 		
 		case "success":
-			return "<div class='alert alert-success'>".$msg. "</div>";
+			if ( $_vars["runType"] == "web" ) {
+				return "<div class='alert alert-success'>".$msg. "</div>";
+			}
+			if ( $_vars["runType"] == "console" ) {
+				return $msg."\n";
+			}
 		break;
 		
 		default:
-			return "<div class='alert alert-info'>".$msg. "</div>";
+			if ( $_vars["runType"] == "web" ) {
+				return "<div class='alert alert-info'>".$msg. "</div>";
+			}
+			if ( $_vars["runType"] == "console" ) {
+				return $msg."\n";
+			}
 		break;
 	}//end switch
 
 }//end logAlert()
 
-?>
 
+function formPageHeader(){
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test PHP</title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Expires" content="0">	
+	<meta http-equiv="X-UA-Compatible" content="IE=10">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">	
+<style>
+.alert {
+	margin-bottom: 3px !important;
+	padding: 10px !important;
+}
+.panel-body{
+	padding: 5px;
+}
+</style>
+</head>
+
+<body>
+	<div class="container">
+		<div class="page-header">
+			<h3>test PHP: OOP</h3>
+		</div>
+<pre>
+// run local web-server
+php -S localhost:8000 -t test_code/php
+
+Мэтт Зандстра, PHP. Объекты, шаблоны и методики программирования (2-е издание)
+php._obekty_shablony_i_metodiki_programmirovaniya.djvu
+p82
+
+https://www.php.net/manual/ru/language.oop5.overloading.php
+Перегрузка 
+
+</pre>
+	<div class="panel">
+		<div class="panel-body">
+';
+}//end formPageHeader
+
+
+function formPageFooter(){
+echo '
 		</div>
 	</div><!-- end panel -->
 </div><!-- end container -->
 
 </body>
 </html>
+';
+}//end formPageFooter
 
+?>
