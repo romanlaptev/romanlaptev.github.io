c56624aa63f020f4693359cd5256079ec996f211
romanlaptev <roman-laptev@yandex.ru>
2020-03-18 09:55:19 +0700

    update test code, php

---
+++ test_code/php/test_namespace.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace space1 {
+	class Class1 {
+
+		private $msg="";
+
+		public function __construct() {
+			$this->msg = "Object of the ". __CLASS__ ." was created.";
+	echo $this->msg."\n";
+		}//end
+
+	}//end class
+
+	$obj1 = new Class1();
+
+	$some_var = 123;
+	function func1( $num ){
+		return $num++;
+	}//end
+	echo "Test name space 'space1': ".func1( $some_var )."\n";
+	
+}//end namespace
+
+
+namespace space2 {
+	class Class1 {
+
+		private $msg="";
+		public $object_ns = "this variable from space2";
+
+		public function __construct() {
+			$this->msg = "Object of the ". __CLASS__ ." was created.";
+	echo $this->msg."\n";
+		}//end
+
+	}//end class
+
+	$obj1 = new Class1();
+//print_r( $obj1 );
+
+	$some_var = 456;
+	function func1( $num ){
+		return $num++;
+	}//end
+	echo "Test name space 'space2': ".func1( $some_var )."\n";
+
+}//end namespace
+
+namespace space2\subspace1 {
+	class Class1 {
+
+		public function __construct() {
+			$msg = "Object of the ". __CLASS__ ." was created.";
+echo $msg."\n";
+		}//end
+
+	}//end class
+
+	$obj1 = new Class1();
+print_r( $obj1 );
+//echo $obj1->object_ns."\n";
+
+}//end namespace
+
+?>
