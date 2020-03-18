4878e06278418ae4d6fa8c029747d596639a8257
romanlaptev <roman-laptev@yandex.ru>
2020-03-18 12:30:35 +0700

    update test code, php

--- test_code/php/test_namespace.php
+++ test_code/php/test_namespace.php
@@ -8,6 +8,11 @@ namespace  {//global_space
 	//echo space2\get_var()."\n";
 	echo namespace\space2\get_var()."\n";
 
+	$obj_space1 = new space1\Class1("obj_space1");
+//print_r( $obj_space1 );
+	$obj_space2 = new space2\Class1("obj_space2");
+	$obj_subSpace2 = new space2\subspace1\Class1("obj_subSpace2");
+
 }//end namespace
 
 
@@ -18,14 +23,14 @@ namespace space1 {
 		private $msg="";
 		public $object_ns = "object_ns, this variable from space1";
 
-		public function __construct() {
-			$this->msg = "Object of the ". __CLASS__ ." was created.";
+		public function __construct($name) {
+			$this->msg = "Object '$".$name."' of the ". __CLASS__ ." was created.";
 	echo $this->msg."\n";
 		}//end
 
 	}//end class
 
-	$obj1 = new Class1();
+	$obj1 = new Class1("obj1");
 //-----------------
 	
 	$some_var = 123;
@@ -48,7 +53,7 @@ namespace space1 {
 	$global_var = 0;
 	echo \space2\subspace1\test()."\n";
 
-}//end namespace
+}//end space 1
 
 
 namespace space2 {
@@ -57,14 +62,14 @@ namespace space2 {
 		private $msg="";
 		public $object_ns = "object_ns, this variable from space2";
 
-		public function __construct() {
-			$this->msg = "Object of the ". __CLASS__ ." was created.";
+		public function __construct($name) {
+			$this->msg = "Object '$".$name."' of the ". __CLASS__ ." was created.";
 	echo $this->msg."\n";
 		}//end
 
 	}//end class
 
-	$obj1 = new Class1();
+	$obj1 = new Class1("obj1");
 //print_r( $obj1 );
 
 	$some_var = 456;
@@ -87,19 +92,20 @@ namespace space2 {
 	echo "-- constant from another namespace: ".\space1\CONST1 ."\n";
 
 
-}//end namespace
+}//end space 2
 
 namespace space2\subspace1 {
 	class Class1 {
+		private $msg;
 
-		public function __construct() {
-			$msg = "Object of the ". __CLASS__ ." was created.";
-echo $msg."\n";
+		public function __construct($name) {
+			$this->msg = "Object '$".$name."' of the ". __CLASS__ ." was created.";
+echo $this->msg."\n";
 		}//end
 
 	}//end class
 
-	$obj1 = new Class1();
+	$obj1 = new Class1("obj1");
 //print_r( $obj1 );
 //echo \space2\$some_var."\n";
 
