a20a52f9ba8a3a1e538605aaa59aca7cc7a6ce0c
romanlaptev <roman-laptev@yandex.ru>
2021-05-12 08:02:48 +0700

    update taransport webapp

--- webapps/transport_app/js/app.js
+++ webapps/transport_app/js/app.js
@@ -1103,8 +1103,8 @@ console.log( "Warn! error parse url in " + target.href );
 
 //------------------		
 	webApp.vars["weatherAPI"]["selectApiName"].addEventListener("change", function(e){
-		var apiName = e.target.selectedOptions[0].value;
-console.log(apiName);
+		var dataSet = e.target.selectedOptions[0].dataset;
+console.log(dataSet);
 	});//end event
 
 //------------------		
