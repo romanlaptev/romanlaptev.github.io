cb10a4bb5747dfac618395e4c045f338e4baf80b
romanlaptev <roman-laptev@yandex.ru>
2025-01-27 11:32:10 +0700

    27-01-2025.1

--- projects/test_landing/page/js/main.js
+++ projects/test_landing/page/js/main.js
@@ -7,6 +7,15 @@ console.log(e.type);
 //console.log(msg);
 
 	landingObj["cardCarousel"] = initCardCarousel();
+	var intervalId = setInterval(function(){
+	  landingObj.cardCarousel.nextSlide();
+	}, 4000);
+
+	document.addEventListener('keydown', event => {
+	  if (event.key === "Escape" || event.keyCode === 27) {
+		clearInterval(intervalId);
+	  }
+	});
 	
 },false);//end dom load
 
@@ -23,8 +32,10 @@ console.error("error, not found cardCarousel");
 		btnPrev: document.querySelector("#card-carousel-btn-prev"),
 		counterCurr: document.querySelector("#card-carousel-current"),
 		counterTotal: document.querySelector("#card-carousel-total"),
-		counter:null,
+		counter:1,
 		//total:null,
+		"nextSlide":_nextSlide,
+		"prevSlide":_prevSlide,
 	}
 	
 	var slides = cardCarousel["carousel"].querySelectorAll(".card");
@@ -32,43 +43,65 @@ console.error("error, not found cardCarousel");
 console.error("error, not found cardCarousel slides...");
 		return false;
 	}
-	cardCarousel["counterCurr"].textContent = 3;
-	cardCarousel["counterTotal"].textContent = slides.length;
 
 	//detect total number of visible slides
 	cardCarousel["numVisibleSlides"] = 0;
 	for(var n=0; n < slides.length; n++){
 //console.log(n,slides[n])
 		var elm = slides[n];
-		if(!elm.classList.contains("d-none")){
-			cardCarousel["numVisibleSlides"]++;
+//console.log(n,elm.getBoundingClientRect().width);
+		//if(elm.classList.contains("d-none")){
+		if(elm.getBoundingClientRect().width === 0){
+			continue;
 		}
+		cardCarousel["numVisibleSlides"]++;
 	}//next	
+
+	cardCarousel["counter"] = cardCarousel["numVisibleSlides"];
+	cardCarousel["counterCurr"].textContent = cardCarousel["numVisibleSlides"];
+	cardCarousel["counterTotal"].textContent = slides.length;
 	
 	if(cardCarousel["btnNext"]){
 		cardCarousel["btnNext"].addEventListener("click",function(e){
 //console.log(e);
-			nextSlide();
+			_nextSlide();
 		});
 	}
 	if(cardCarousel["btnPrev"]){
 		cardCarousel["btnPrev"].addEventListener("click",function(e){
 //console.log(e);			
-			//cardCarousel["counter"]--;
-			//cardCarousel["counterCurr"].textContent = cardCarousel["counter"];
-			prevSlide();
+			cardCarousel["counter"]--;
+			if(cardCarousel["counter"] < 1){
+				cardCarousel["counter"] = slides.length;
+			}
+			cardCarousel["counterCurr"].textContent = cardCarousel["counter"];
+			_prevSlide();
 		});
 	}
 	return cardCarousel;
 }//
 
 
-function nextSlide(){
-	var slides = landingObj["cardCarousel"]["carousel"].querySelectorAll(".card");
+function _nextSlide(){
+	
+	var cardCarousel = landingObj["cardCarousel"];
+	if(!cardCarousel){
+console.error("error, not found cardCarousel");
+		return false;
+	}
+//console.log(cardCarousel);
+	
+	var slides = cardCarousel["carousel"].querySelectorAll(".card");
 	if(!slides){
 console.error("error, not found cardCarousel slides...");
 		return false;
 	}
+
+	cardCarousel["counter"]++;
+	if(cardCarousel["counter"] > slides.length){
+		cardCarousel["counter"] = cardCarousel["numVisibleSlides"];
+	}
+	cardCarousel["counterCurr"].textContent = cardCarousel["counter"];
 	
 	//hide first visible slide
 	var num = 0;
@@ -81,39 +114,43 @@ console.error("error, not found cardCarousel slides...");
 	
 	//show next slide (num + 3)
 	//slides[3].classList.remove("d-none");
-	var num = num + landingObj["cardCarousel"]["numVisibleSlides"];
-	slides[num].classList.remove("d-none");
-	
-	//landingObj["cardCarousel"]["counterCurr"].textContent = landingObj["cardCarousel"]["numFirstVisibleSlide"];
+	num = cardCarousel["numVisibleSlides"];
+//console.log(num);
+	slides[num].classList.remove("d-none","d-none-375");
 	
 }//
 
-function prevSlide(){
-	var slides = landingObj["cardCarousel"]["carousel"].querySelectorAll(".card");
+function _prevSlide(){
+
+	var cardCarousel = landingObj["cardCarousel"];
+	if(!cardCarousel){
+console.error("error, not found cardCarousel");
+		return false;
+	}
+
+	var slides = cardCarousel["carousel"].querySelectorAll(".card");
 //console.log(slides);
 	if(!slides){
 console.error("error, not found cardCarousel slides...");
 		return false;
 	}
-	//move last slide to the start slides
+	
+	//move and show last slide to the start position slides
 	//slides[0].before("BEFORE!!!");
-
 	var endNum = slides.length-1;
 	slides[0].before(slides[endNum]);
-	slides[endNum].classList.remove("d-none");
+	slides[endNum].classList.remove("d-none","d-none-375");
+	
+	//refresh slides (because was changed slides order!!!)
+	slides = cardCarousel["carousel"].querySelectorAll(".card");
+
+	//hide last (pre-first) slide 
+	var endNum = slides.length-1;
+	slides[endNum].classList.add("d-none");
 
 	//hide next slide (num + 3)
-	slides[2].classList.add("d-none");
-	var num = num + landingObj["cardCarousel"]["numVisibleSlides"];
+	//slides[2].classList.add("d-none");
+	var num = cardCarousel["numVisibleSlides"];
 	slides[num].classList.add("d-none");
-/*	
-	//refresh slides (change slides order!!!)
-	slides = landingObj["cardCarousel"]["carousel"].querySelectorAll(".card");
-
-	//move and hide second visible slide to the end slides
-	var endNum = slides.length-1;
-	slides[endNum].after(slides[1]);
-	slides[1].classList.add("d-none");
-*/
 	
 }//
